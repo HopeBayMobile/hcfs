ad9a9dd1f217da41bd163c9f1dbfeed52329557f
Ken Liu <forken.liu@delta.com.tw>
2012-04-13 13:52:58 +0800

    Implement lockfile for CmdReceiver

--- DCloudSwift/proxy/CmdReceiver.py
+++ DCloudSwift/proxy/CmdReceiver.py
@@ -2,11 +2,7 @@
 Created on 2012/03/01
 
 @author: CW
-
-Modified by Ken on 2012/03/12
-Modified by Ken on 2012/03/13
-Modified by CW on 2012/03/22: correct the absolute path of function triggerProxyDeploy()
-Modified by Ken on 2012/04/09: add triggerFirstProxy
+Modified by Ken on 2012/04/13: add lockfile
 '''
 
 import sys
@@ -18,6 +14,7 @@ import json
 import subprocess
 import shlex
 import random
+import fcntl
 from decimal import *
 from datetime import datetime
 from ConfigParser import ConfigParser
@@ -36,11 +33,14 @@ Options:
 Examples:
 	python CmdReceiver.py -p {"password": "deltacloud"}
 '''
+EEXIST = 17
+lockFile = "/tmp/CmdReceiver.lock"
+
+class UsageError(Exception):
+	pass
 
 def usage():
 	print >> sys.stderr, Usage
-	sys.exit(1)
-
 
 def triggerAddStorage(**kwargs):
 	logger = util.getLogger(name="triggerAddStorage")
@@ -129,57 +129,60 @@ def triggerRmStorage(**kwargs):
 	return (returncode, blackProxyNodes, blackStorageNodes)
 
 def main():
-	if not util.isAllDebInstalled("/DCloudSwift/proxy/deb_source/"):
-		util.installAllDeb("/DCloudSwift/proxy/deb_source/")
+	returncode =0
+	fd = -1
+	try:
+		fd = os.open(lockFile, os.O_RDWR| os.O_CREAT | os.O_EXCL, 0444)
 
-	if not util.findLine("/etc/ssh/ssh_config", "StrictHostKeyChecking no"):
-		os.system("echo \"    StrictHostKeyChecking no\" >> /etc/ssh/ssh_config")
+		if not util.isAllDebInstalled("/DCloudSwift/proxy/deb_source/"):
+			util.installAllDeb("/DCloudSwift/proxy/deb_source/")
 
-	if (len(sys.argv) == 3 ):
-		kwargs = None
-        	if (sys.argv[1] == 'addStorage' or sys.argv[1] == '-a'):
-			try:
+		if not util.findLine("/etc/ssh/ssh_config", "StrictHostKeyChecking no"):
+			os.system("echo \"    StrictHostKeyChecking no\" >> /etc/ssh/ssh_config")
+
+		if (len(sys.argv) == 3 ):
+			kwargs = None
+        		if (sys.argv[1] == 'addStorage' or sys.argv[1] == '-a'):
 				kwargs = json.loads(sys.argv[2])
-			except ValueError:
-				print >> sys.stderr, "Usage error: Ivalid json format"
-				usage()
-
-			print 'AddStorage start'
-			(returncode, blackProxy, blackStorage) = triggerAddStorage(**kwargs)
-		elif (sys.argv[1] == 'rmStorage' or sys.argv[1] == '-r'):
-                        try:
-                                kwargs = json.loads(sys.argv[2])
-                        except ValueError:
-                                print >> sys.stderr, "Usage error: Ivalid json format"
-                                usage()
-
-                        print 'Proxy deployment start'
-                        triggerRmStorage(**kwargs)	
-
-        	elif (sys.argv[1] == 'firstProxy' or sys.argv[1] == '-f'):
-			try:
+				print 'AddStorage start'
+				triggerAddStorage(**kwargs)
+			elif (sys.argv[1] == 'rmStorage' or sys.argv[1] == '-r'):
+                               	kwargs = json.loads(sys.argv[2])
+                        	print 'Proxy deployment start'
+                        	triggerRmStorage(**kwargs)	
+        		elif (sys.argv[1] == 'firstProxy' or sys.argv[1] == '-f'):
 				kwargs = json.loads(sys.argv[2])
-			except ValueError:
-				print >>sys.stderr,  "Usage error: Ivalid json format"
-				usage()
-
-			print 'First proxy deployment start'
-			triggerFirstProxyDeploy(**kwargs)
-
-        	elif (sys.argv[1] == 'proxy' or sys.argv[1] == '-p'):
-			try:
+				print 'First proxy deployment start'
+				triggerFirstProxyDeploy(**kwargs)
+        		elif (sys.argv[1] == 'proxy' or sys.argv[1] == '-p'):
 				kwargs = json.loads(sys.argv[2])
-			except ValueError:
-				print >>sys.stderr,  "Usage error: Ivalid json format"
-				usage()
-
-			print 'Proxy deployment start'
-			triggerProxyDeploy(**kwargs)
+				print 'Proxy deployment start'
+				triggerProxyDeploy(**kwargs)
+			else:
+				print >> sys.stderr, "Usage error: Invalid optins"
+                		raise UsageError
+        	else:
+			raise UsageError
+	except OSError as e:
+		if e.errno == EEXIST:
+			print >>sys.stderr, "A confilct task is in execution"
 		else:
-			print >> sys.stderr, "Usage error: Invalid optins"
-                	usage()
-        else:
+			print >>sys.stderr, str(e)
+		returncode = e.errno
+	except UsageError:
 		usage()
+		returncode = 1
+	except ValueError:
+		print >>sys.stderr,  "Usage error: Ivalid json format"
+		returncode = 1
+	except Exception as e:
+		print >>sys.stderr, str(e)
+		returncode = 1
+	finally:
+		if fd != -1:
+			os.close(fd)
+			os.unlink(lockFile)
+		sys.exit(returncode)
 
 
 if __name__ == '__main__':
