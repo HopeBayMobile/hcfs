a6773bdd36c840bc8042ae604cfaf23dc9842b1a
Forken <f92019@gmail.com>
2012-04-22 19:58:02 +0800

    Change to use read stdout before wait

--- DCloudSwift/misc/BootScripts/rc.py
+++ DCloudSwift/misc/BootScripts/rc.py
@@ -8,10 +8,132 @@ import pickle
 import socket
 import logging
 import threading
+import re
 
 FORMATTER = '[%(levelname)s from %(name)s on %(asctime)s] %(message)s'
 
-def getLogger(name=None):
+def __loadSwiftMetadata(disk):
+        logger = getLogger(name="__loadSwiftMetadata")
+	metadata = {}
+        mountpoint = "/temp/%s"%disk
+        os.system("mkdir -p %s"%mountpoint)
+
+
+	#TODO: chechsum
+	if mountDisk(disk, mountpoint) !=0:
+                logger.error("Failed to mount %s"%disk)
+                return 1
+
+	returncode = 0
+
+	cmd = "cp %s/swift/* /etc/swift/"%(mountpoint)
+	po = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	output = po.stdout.read()
+        po.wait()
+
+        if po.returncode != 0:
+                logger.error("Failed to reload swift metadata from %s for %s"%(disk,output))
+		returncode = 1
+
+	if lazyUmount(mountpoint)!=0:
+		logger.warn("Failed to umount disk %s from %s"%(disk, mountpoint))
+
+	return returncode
+
+def getAllDisks():
+	cmd = "fdisk -l"
+	po = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	lines = po.stdout.readlines()
+
+	disks = []
+	for line in lines:
+	match = re.match(r"^Disk /dev/sd\w:", line)
+	if match is not None:
+		disks.append(line.split()[1][:8])
+
+	return disks
+	
+def getRootDisk():
+	cmd = "mount"
+	po = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	lines = po.stdout.readlines()
+
+	device = lines[0].split()[0]
+	return device[:8]
+
+
+
+def getLatestMetadata():
+	logger = getLogger(name="getLatestMetadata")
+	logger.info("start")
+	disks = getNonRootDisks()
+	latestMetadata = None
+
+       	for disk in disks:
+		(ret, metadata) = readMetadata(disk)
+		if ret == 0:
+			latestMetadata  = metadata  if latestMetadata is None or latestMetadata["vers"] < metadata["vers"]  else latestMetadata
+			
+
+	logger.info("end")
+	return latestMetadata
+
+def getNonRootDisks():
+	rootDisk = getRootDisk()
+	disks = getAllDisks()
+	nonRootDisks =[]
+
+	for disk in disks:
+		if disk != rootDisk:
+			nonRootDisks.append(disk)
+
+	return nonRootDisks
+
+
+
+def lazyUmount(mountpoint):
+        logger = getLogger(name="umount")
+
+        returncode = 0
+        try:
+		if os.path.ismount(mountpoint):
+			cmd = "umount -l %s"%mountpoint
+			po = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+			output = po.stdout.read()
+                 	po.wait()
+
+                 	if po.returncode !=0:
+                 		logger.error("Failed to umount -l %s for %s"%(mountpoint, output))
+				returncode = 1
+
+        except OSError as e:
+                logger.error("Failed to umount -l %s for %s"%(disk, e))
+		returncode = 1
+
+        return returncode
+
+def loadSwiftMetadata():
+	logger = getLogger(name="loadSwiftMetadata")
+	logger.info("start")
+
+        disks = getNonRootDisks()
+	latestMetadata = getLatestMetadata()
+	os.system("mkdir -p /etc/swift")
+	returncode = 1
+
+        for disk in disks:
+		(ret, metadata) = readMetadata(disk)
+		if ret==0 and metadata["vers"] >= latestMetadata["vers"]:
+			if __loadSwiftMetadata(disk) ==0:
+				returncode = 0
+				break
+
+	os.system("chown -R swift:swift /etc/swift")
+
+	logger.info("end")
+	return returncode
+	
+	def getLogger(name=None):
 	"""
 	Get a file logger using config settings.
 
@@ -45,64 +167,33 @@ def getLogger(name=None):
 		return logger
 	finally:
 		pass
-
-def getRootDisk():
-	cmd = "mount"
-	po  = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	po.wait()
-
-	firstLine = po.stdout.readline()
-	device = firstLine.split()[0]
-	return device[:8]
-
-def getAllDisks():
-	cmd = "fdisk -l|grep \"Disk /dev\" "
-	po  = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	po.wait()
-
-	disks = []
-	for line in po.stdout.readlines():
-		disks.append(line.split()[1][:8])
-
-	return disks
-
-def getNonRootDisks():
-	rootDisk = getRootDisk()
-	disks = getAllDisks()
-	nonRootDisks =[]
-
-	for disk in disks:
-		if disk != rootDisk:
-			nonRootDisks.append(disk)
-
-	return nonRootDisks
-		
-
+	
 def mountDisk(disk, mountpoint):
 	logger = getLogger(name="mountDisk")
 
-	returncode = 1
+	returncode = 0
 	try:
 		os.system("mkdir -p %s"%mountpoint)
 		if os.path.ismount(mountpoint):
-                	os.system("umount -l %s"%mountpoint)
+			os.system("umount -l %s"%mountpoint)
 
 		#TODO: Add timeout mechanism
                 cmd = "mount %s %s"%(disk, mountpoint)
-                po = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                po = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		output = po.stdout.read()
                 po.wait()
 
                 if po.returncode !=0:
-                        logger.error("Failed to mount  %s for %s"%(disk, po.stderr.readline()))
-
-		returncode = 0
+                        logger.error("Failed to mount %s for %s"%(disk, output))
+                        returncode =1
 
         except OSError as e:
                 logger.error("Failed to mount %s for %s"%(disk, e))
+                returncode = 1
 
 	return returncode
 
-		
+
 def readMetadata(disk):
         logger = getLogger(name="readMetadata")
 	metadata = {}
@@ -126,88 +217,6 @@ def readMetadata(disk):
 	finally:
 		if lazyUmount(mountpoint)!=0:
 			logger.warn("Failed to umount disk %s from %s"%(disk, mountpoint))
-
-
-def getLatestMetadata():
-	logger = getLogger(name="getLatestMetadata")
-	logger.info("start")
-	disks = getNonRootDisks()
-	latestMetadata = None
-
-       	for disk in disks:
-		(ret, metadata) = readMetadata(disk)
-		if ret == 0:
-			latestMetadata  = metadata  if latestMetadata is None or latestMetadata["vers"] < metadata["vers"]  else latestMetadata
-			
-
-	logger.info("end")
-	return latestMetadata
-
-def __loadScripts(disk):
-        logger = getLogger(name="__loadSripts")
-	metadata = {}
-        mountpoint =  "/temp/%s"%disk
-        os.system("mkdir -p %s"%mountpoint)
-
-
-	#TODO: chechsum
-	if mountDisk(disk, mountpoint) !=0:
-                logger.error("Failed to mount %s"%disk)
-                return 1
-
-	returncode = 0
-
-	cmd = "cp -r %s/DCloudSwift /"%(mountpoint)
-	po  = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        po.wait()
-        if po.returncode != 0:
-                logger.error("Failed to reload scripts from %s for %s"%(disk,po.stderr.readline()))
-		returncode = 1
-
-	if lazyUmount(mountpoint)!=0:
-		logger.warn("Failed to umount disk %s from %s"%(disk, mountpoint))
-
-	return returncode
-
-def loadScripts():
-	logger = getLogger(name="loadSripts")
-	logger.info("start")
-
-        disks = getNonRootDisks()
-	latestMetadata = getLatestMetadata()
-	os.system("mkdir -p /etc/swift")
-	returncode = 1
-
-        for disk in disks:
-		(ret, metadata) = readMetadata(disk)
-		if ret==0 and metadata["vers"] >= latestMetadata["vers"]:
-			if __loadScripts(disk) ==0:
-				returncode = 0
-				break
-
-	logger.info("end")
-	return returncode
-
-
-def lazyUmount(mountpoint):
-        logger = getLogger(name="umount")
-
-        returncode = 0
-        try:
-		if os.path.ismount(mountpoint):
-                	cmd = "umount -l %s"%mountpoint
-                	po = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                	po.wait()
-
-                	if po.returncode !=0:
-                        	logger.error("Failed to umount -l  %s for %s"%(mountpoint, po.stderr.readline()))
-				returncode = 1
-
-        except OSError as e:
-                logger.error("Failed to umount -l %s for %s"%(disk, e))
-		returncode = 1
-
-        return returncode
 
 
 def main(argv):
