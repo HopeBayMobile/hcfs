d2082247867c5405c77292f0568816a951ce8e3d
willy-wu <willy.wu@delta.com.tw>
2012-05-08 18:22:18 +0800

    Add new IPAddressInput Widget for network address input. Remove masked input plugin beacuse of improper functions.

--- StorageGateway/wizard/forms.py
+++ StorageGateway/wizard/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.forms.widgets import Input
+from django.forms.util import flatatt
+from django.utils.safestring import mark_safe
+from django.utils.encoding import StrAndUnicode, force_unicode
 from lib.forms import RenderFormMixinClass
 from lib.forms.widgets import *
 
@@ -59,17 +63,60 @@ class Form_4(RenderFormMixinClass, forms.Form):
                                         ]
                  )]
     
+class IPAddressInput(Input):
+    input_type = 'text'
+    separator = u'.'
+    input_number = 4
+
+    def value_from_datadict(self, data, files, name):
+        """
+        Given a dictionary of data and this widget's name, returns the value
+        of this widget. Returns None if it's not provided.
+        """
+        input_list = []
+        for i in range(self.input_number):
+            input = name + u'[%s]' % i
+            input_list.append(data.get(input, None))
+        data = self.separator.join(input_list)
+        
+        if any(input_list):
+            return data
+        else:
+            return None
+    
+    def render(self, name, value, attrs=None):
+        if value is None:
+            value = ''
+        final_attrs = self.build_attrs(attrs, type=self.input_type)
+        final_attrs['class'] = 'span05'
+        
+        value_list = [''] * self.input_number
+        if value != '':
+            value_list = value.split(self.separator)
+
+        #create 4 input for IP address
+        input_list = []
+        for i in range(self.input_number):
+            final_attrs['name'] = name + (u'[%s]' % i)
+            final_attrs['value'] = force_unicode(self._format_value(value_list[i]))
+            if not value_list[i]:
+                del final_attrs['value']
+            input_list.append(u'<input%s />' % flatatt(final_attrs))
+        render_mark = self.separator.join(input_list)
+        
+        return mark_safe(render_mark)
+ 
 class Form_All(RenderFormMixinClass, forms.Form):
 #    username = forms.CharField()
 #    password = forms.CharField(widget=forms.PasswordInput())
     new_password = forms.CharField(widget=forms.PasswordInput())
     retype_new_password = forms.CharField(widget=forms.PasswordInput())
     
-    ip_address = forms.IPAddressField(label='IP address')
-    subnet_mask = forms.IPAddressField()
-    default_gateway = forms.IPAddressField()
-    preferred_dns = forms.IPAddressField(label='Preferred DNS server')
-    alternate_dns = forms.IPAddressField(label='Alternate DNS server')
+    ip_address = forms.IPAddressField(label='IP address', widget=IPAddressInput)
+    subnet_mask = forms.IPAddressField(widget=IPAddressInput)
+    default_gateway = forms.IPAddressField(widget=IPAddressInput)
+    preferred_dns = forms.IPAddressField(label='Preferred DNS server', widget=IPAddressInput)
+    alternate_dns = forms.IPAddressField(label='Alternate DNS server', widget=IPAddressInput)
     
     cloud_storage_ip = forms.IPAddressField(label='Cloud Storage IP')
     cloud_storage_account = forms.CharField()
