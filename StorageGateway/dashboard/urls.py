b0d4a16cf812030bbea772ff9a33940243f2c300
jsleetw <ail529@gmail.com>
2012-05-09 17:28:15 +0800

    add sync_management system_log

--- StorageGateway/dashboard/urls.py
+++ StorageGateway/dashboard/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns("dashboard.views",
     url(r'^$', "index"),
     url(r'^account_configuration$', "account_configuration"),
+    url(r'^sync_management$', "sync_management"),
+    url(r'^system_log$', "system_log"),
     url(r'^login$', "login")
 )
--- StorageGateway/dashboard/views.py
+++ StorageGateway/dashboard/views.py
@@ -8,3 +8,11 @@ def index(request):
 @login_required
 def account_configuration(request):
     return render(request, 'account_configuration.html')
+
+@login_required
+def sync_management(request):
+    return render(request, 'sync_management.html')
+
+@login_required
+def system_log(request):
+    return render(request, 'system_log.html')
