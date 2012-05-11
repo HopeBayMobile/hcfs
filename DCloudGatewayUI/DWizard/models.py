4baf5120b052055cbed6885243350d28f84489de
willy-wu <willy.wu@delta.com.tw>
2012-05-11 17:20:57 +0800

    Add new Work and Step Model to store wizard status

--- DCloudGatewayUI/DWizard/models.py
+++ DCloudGatewayUI/DWizard/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+class Work(models.Model):
+    work_name = models.CharField(max_length=255, unique=True)
+    current_form = models.CharField(max_length=255)
+
+class Step(models.Model):
+    wizard = models.ForeignKey(Work)
+    form = models.CharField(max_length=255)
+    data = models.TextField()
+    task_id = models.CharField(max_length=255)
