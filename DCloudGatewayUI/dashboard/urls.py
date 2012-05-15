c7434aa7cf447eac26042a2ce6d4132f585eba22
Jim Yeh <lemonlatte@gmail.com>
2012-05-15 15:52:34 +0800

    Build out all tabs of storage gateway. Build out form views for the account page.

--- DCloudGatewayUI/dashboard/urls.py
+++ DCloudGatewayUI/dashboard/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns("dashboard.views",
     url(r'^$', "index"),
-    url(r'^account_configuration$', "account_configuration"),
+    url(r'^account$', "account"),
     url(r'^system$', "system"),
-    url(r'^sync_management$', "sync_management"),
-    url(r'^system_log$', "system_log"),
+    url(r'^sync$', "sync"),
+    url(r'^sharefolder$', "sharefolder"),
+    url(r'^syslog$', "syslog"),
+    url(r'^power$', "power"),
     url(r'^login$', "login")
 )
