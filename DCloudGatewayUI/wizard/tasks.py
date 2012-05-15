deb018a7a9a881aed41cd6c77107f8ff3d0144f8
willy-wu <willy.wu@delta.com.tw>
2012-05-15 13:18:36 +0800

    Update task of installation.

--- DCloudGatewayUI/wizard/tasks.py
+++ DCloudGatewayUI/wizard/tasks.py
@@ -4,15 +4,18 @@ from lib.models.config import Config
 import time
 
 @task
-def install_task():
+def install_task(data):
+    #save data in lib_config table
+    for k, v in data.iteritems():
+        c = Config(key=k, value=v)
+        c.save()
+    
     #Step 1, save password of administrator
     new_password = Config.objects.get(key='new_password')
     admin = User.objects.get(username='admin')
     admin.set_password(new_password.value)
     admin.save()
-    #update status
-    Config.objects.create(key='step_1', value="{'result': True, 'msg': 'string step_1'}") 
-    
+        
     #Step 2, apply network settings
     ip_address = Config.objects.get(key='ip_address')
     subnet_mask = Config.objects.get(key='subnet_mask')
@@ -21,24 +24,20 @@ def install_task():
     alternate_dns = Config.objects.get(key='alternate_dns')
     
     #call API: apply_network
-    time.sleep(7)
-    Config.objects.create(key='step_2', value="{'result': True, 'msg': 'string step_2'}") 
-    
+        
     #Step 3, cloud storage settings
     cloud_storage_url = Config.objects.get(key='cloud_storage_url')
     cloud_storage_account = Config.objects.get(key='cloud_storage_account')
     cloud_storage_password = Config.objects.get(key='cloud_storage_password')
     
     #call API: test_storage_account
-    time.sleep(7)
-    Config.objects.create(key='step_3', value="{'result': True, 'msg': 'string step_3'}") 
     
     #Step 4, encryption key
     encryption_key = Config.objects.get(key='encryption_key')
     
     #call API: apply_user_enc_key
-    time.sleep(7)
-    Config.objects.create(key='step_4', value="{'result': True, 'msg': 'string step_4'}") 
+    
+    
     time.sleep(7)
     return True
 
