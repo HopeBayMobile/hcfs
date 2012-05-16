157b7abc1ad1f698c47f9898b3872bc50bdeec31
Jim Yeh <lemonlatte@gmail.com>
2012-05-16 16:22:49 +0800

    Remove the soft link of gateway module. You should install gateway through setup.py in DCloudGateway. Update the structure of forms_group. Link gateway api to account view and system view.

--- DCloudGatewayUI/dashboard/urls.py
+++ DCloudGatewayUI/dashboard/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns("dashboard.views",
-    url(r'^$', "index"),
-    url(r'^account$', "account"),
-    url(r'^system$', "system"),
-    url(r'^sync$', "sync"),
-    url(r'^sharefolder$', "sharefolder"),
-    url(r'^syslog$', "syslog"),
-    url(r'^power$', "power"),
-    url(r'^login$', "login")
+    url(r'^$', "index", name="index"),
+    url(r'^account/(?P<action>\w*)$', "account", name="account"),
+    url(r'^system/(?P<action>\w*)$', "system", name="system"),
+    url(r'^sync/$', "sync", name="sync"),
+    url(r'^sharefolder/$', "sharefolder", name="sharefolder"),
+    url(r'^syslog/$', "syslog", name="syslog"),
+    url(r'^power/$', "power", name="power"),
 )
