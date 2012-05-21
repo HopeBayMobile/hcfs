9c3ef3c38cbc5d9c82c3878f8d1716b6ac76789e
Willy-Wu <willy.wu@delta.com.tw>
2012-05-21 16:21:52 +0800

    Update installation foms for quickly testing.

--- DCloudGatewayUI/wizard/urls.py
+++ DCloudGatewayUI/wizard/urls.py
@@ -1,22 +1,44 @@
 from django.conf.urls.defaults import patterns, url
 from DWizard.views import DeltaWizard
-from forms import Form_All
+from forms import Form_All,Test_Form_All
 from tasks import install_task
 
 
 class InstallWizard(DeltaWizard):
+    template_name = 'wizard/form.html'
     wizard_step = [
-        (Form_All, install_task)
+#        (Form_All, install_task)
+        (Test_Form_All, install_task)
     ]
+#    wizard_initial = {
+#        'Form_All': {
+#            'ip_address': '192.168.0.1',
+#            'subnet_mask': '255.255.255.0',
+#            'default_gateway': '192.168.0.254',
+#            'preferred_dns': '8.8.8.8',
+#            'alternate_dns': '8.8.4.4',
+#
+#            'cloud_storage_url': '172.168.288.53:8080'
+#        }
+
+    #Testing Usage
     wizard_initial = {
-        'Form_All': {
-            'ip_address': '192.168.0.1',
+        'Test_Form_All': {
+            'new_password': '1234',
+            'retype_new_password':'1234',
+                     
+            'ip_address': '171.16.229.154',
             'subnet_mask': '255.255.255.0',
-            'default_gateway': '192.168.0.254',
+            'default_gateway': '172.16.229.1',
             'preferred_dns': '8.8.8.8',
             'alternate_dns': '8.8.4.4',
 
-            'cloud_storage_url': '172.168.288.53:8080'
+            'cloud_storage_url': '172.168.288.53:8080',
+            'cloud_storage_account': 'system.root',
+            'cloud_storage_password': 'testpass',
+            
+            'encryption_key':'123456',
+            'confirm_encryption_key':'123456',
         }
     }
 
