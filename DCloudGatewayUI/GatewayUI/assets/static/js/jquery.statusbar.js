94dbc3b79842b34b5b776b615aa69cf689f93c1c
Jim Yeh <lemonlatte@gmail.com>
2012-05-29 10:31:01 +0800

    Relayout the base.html by table. Implement the event message bar through jquery statusbar.

---
+++ DCloudGatewayUI/GatewayUI/assets/static/js/jquery.statusbar.js
@@ -0,0 +1,87 @@
+/**
+ * jquery.statusbar v1.0
+ * copyright 2011 Rob Stortelers (Merula Softworks) - www.merulasoft.nl
+ */
+(function($){
+ $.statusbar = function(options) {
+
+    var defaults = {
+		onClick:function(){} //on message click
+	};
+
+	var options = $.extend(defaults, options);
+
+	var slided = false ; //is the bar slidedown?
+
+	createBar();
+
+	function createBar() //creates the html for the bar
+	{
+		//create the bar
+		var statusbar = $("<div \"style=\";position:relative; top:0px; left:0px; width:100%\" class=\"ui-widget statusbar-wrapper\"><div style=\"overflow:auto;\" class=\"ui-widget-content statusbar-content\"></div><div class=\"ui-widget-header statusbar-header\">Error Message<div class=\"toggle-btn\"></div></div></div>");
+		$('.main-row').prepend(statusbar);
+		// $('html').css("margin-top","26px");
+		$('.statusbar-content').hide();
+		// var btnRemoveAll = $('<button style="margin:5px;" class="statusbar-content-button-remove-all">Remove all</button>').button().click(removeAll);
+		// $('.statusbar-content').append(btnRemoveAll);
+		$('.statusbar-header').hover(function(){$(this).toggleClass('ui-state-hover');});
+		$('.statusbar-header').click(slide);
+
+		//create text message area
+		$('.statusbar-header').append("<div style=\"float:left;margin:5px;\" class=\"statusbar-header-text\">&nbsp;</div>");
+	}
+
+	function slide()//show history of messages
+	{
+		if(!slided)
+		{
+			$('.statusbar-content').height($(".main-row").height() - 70 -$('.statusbar-header').outerHeight()-($('.statusbar-content').outerHeight()-$('.statusbar-content').height()));
+			$('.statusbar-content').slideDown(function(){
+				$('.tab-content').hide();
+				$(".toggle-btn").toggleClass("active");
+			});
+
+		}else
+		{
+			$('.statusbar-content').slideUp();
+			$('.tab-content').show();
+			$(".toggle-btn").toggleClass("active");
+		}
+		slided = !slided;
+	}
+
+	// function removeAll()
+	// {
+	// 	$('.statusbar-content-item').remove();
+	// }
+
+	// this.addMessage = function(message,argument) //add a message to the bar
+	// {
+	// 	$('.statusbar-header-text').clearQueue().show().css("opacity","").fadeOut(function()
+	// 	{
+	// 		$('.statusbar-header-text').text(message).fadeIn();
+	// 		addDetails(message,argument);
+
+	// 	});
+	// }
+
+	// function addDetails(message,argument) //add to message details
+	// {
+	// 	var item = $("<div style=\"margin:5px;padding:20px;cursor:pointer;\" class=\"statusbar-content-item ui-widget-content\"></div>");
+	// 	item.hover(function(){$(this).toggleClass('ui-state-highlight')})
+	// 	item.text(message);
+	// 	var btnDelete = $('<button class="statusbar-content-item-button">Remove</button>');
+	// 	btnDelete.css('position','relative');
+	// 	btnDelete.css('float','right');
+	// 	btnDelete.button();
+	// 	item.append(btnDelete);
+	// 	btnDelete.click(function(){
+	// 		item.remove();
+	// 	})
+	// 	item.click(function(){options.onClick(argument)})
+	// 	$('.statusbar-content>button').after(item);
+	// }
+
+	return this;
+ };
+})(jQuery);
