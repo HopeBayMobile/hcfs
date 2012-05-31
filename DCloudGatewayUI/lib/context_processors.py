0631a29244464bc17fa881ea257b9c23c124f7a0
Jim Yeh <lemonlatte@gmail.com>
2012-05-31 18:13:52 +0800

    Remove initial value of wizard. Fix the refresh error in download/upload value.

--- DCloudGatewayUI/lib/context_processors.py
+++ DCloudGatewayUI/lib/context_processors.py
@@ -5,5 +5,13 @@ import json
 def indicator(request):
 
     indicator_data = json.loads(api.get_gateway_indicators())
+    try:
+        status_data = json.loads(api.get_gateway_status())
+    except:
+        status_data = {'data': {"uplink_usage": 100,
+                                "downlink_usage": 1000
+                               }
+                      }
+    indicator_data['data'].update(status_data['data'])
     print indicator_data
     return {"result_data": indicator_data['data']}
