f0f7f38da3d4763952909e171d6ab715c5faf9bb
yen <yen.kuo@delta.com.tw>
2012-06-19 15:44:35 +0800

    save progress of api_restore_conf.py

--- DCloudGateway/src/http_proxy/api_http_proxy.py
+++ DCloudGateway/src/http_proxy/api_http_proxy.py
@@ -4,25 +4,59 @@
 
 # Function: API function for controlling HTTP proxy 
 
-
 import os
+import json
+import subprocess
 
+def _read_command_log(log_fname):
+	"""
+	Read back an operation log.
+	"""
+	try:
+		fileIN = open(log_fname, "r")
+		fc = fileIN.read()
+		fc = fc.replace("\n"," & ")
+		fc = fc.replace("'","")   # strip ' char
+	except:
+		fc = ''
+
+	return fc
+
+# --------------------------------------------------------------------------------------	
 def set_http_proxy( setting ):
 	"""
 	Toggle squid service to be on or off. Accept an "on" or "off" string as input.
 	@type setting: string
 	"""
+	log_fname = "/tmp/op_log.txt"
+	
 	if setting=="on":
-		# do something here ...
-		op_ok = True
-		op_code = "000"
-		op_msg = None
-
+		cmd = "sudo service squid3 restart > " + log_fname
+		a = os.system(cmd)
+		op_log = _read_command_log(log_fname)
+		# set result values
+		if a==0 and 'fail' not in op_log:
+			op_ok = True
+			op_code = "000"
+			op_msg = None
+		else:
+			op_ok = False
+			op_code = "100"
+			op_msg = "failed to turn on http proxy."
+			
 	if setting=="off":
-		# do something here ...
-		op_ok = True
-		op_code = "000"
-		op_msg = None
+		cmd = "sudo service squid3 stop > " + log_fname
+		a = os.system(cmd)
+		op_log = _read_command_log(log_fname)
+		# set result values
+		if a==0 and 'fail' not in op_log:
+			op_ok = True
+			op_code = "000"
+			op_msg = None
+		else:
+			op_ok = False
+			op_code = "100"
+			op_msg = "failed to turn off http proxy."
 	
 	return_val = {'result'	: op_ok,
         	      'code'	: op_code,
