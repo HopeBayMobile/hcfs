0b1303c912edae0b5677b52d3563c0cab8af5ffd
andy24 <andy.hung@delta.com.tw>
2012-07-02 18:08:17 +0800

    use mock for unit test

--- DCloudGateway/unittest/testRestoreConf.py
+++ DCloudGateway/unittest/testRestoreConf.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import sys
 import nose
 import simplejson as json
 from gateway.SwiftClient import *
 from gateway.api_restore_conf import *
-
+from flexmock import flexmock
 
 class TestRestoreConf:
     '''
     '''
     def setup(self):
+        reload(subprocess)
         pass
 
     def teardown(self):
@@ -18,20 +20,72 @@ class TestRestoreConf:
 
     @nose.tools.raises(SwiftUploadError)
     def testSwiftClientUploadFileNotExist(self):
+        self.mock = flexmock()
+        flexmock(subprocess.Popen).new_instances(self.mock)
+        self.mock.should_receive('wait').and_return(True)
+        self.mock.returncode = '0'
+        self.mock.should_receive('communicate').and_return(["Local file 'mock/etc/network/inter' not found\n", None])
         swiftClient = SwiftClient()
         swiftClient.upload('config', '/etc/network/inter')
 
     @nose.tools.raises(SwiftDeleteError)
     def testSwiftClientDeleteFileNotExist(self):
+        self.mock = flexmock()
+        flexmock(subprocess.Popen).new_instances(self.mock)
+        self.mock.should_receive('wait').and_return(True)
+        self.mock.returncode = '0'
+        self.mock.should_receive('communicate').and_return(["Object 'mock/config/aaaaa' not found\n", None])
         swiftClient = SwiftClient()
         swiftClient.delete('config', 'aaaaa')
 
+    @nose.tools.raises(SwiftCommandError)
+    def testSwiftClientDeleteCmdFail(self):
+        self.mock = flexmock()
+        flexmock(subprocess.Popen).new_instances(self.mock)
+        self.mock.should_receive('wait').and_return(True)
+        self.mock.returncode = '0'
+        self.mock.should_receive('communicate').and_raise(SwiftCommandError())
+        swiftClient = SwiftClient()
+        swiftClient.delete('config', 'aaaaa')
+
+    @nose.tools.raises(SwiftCommandError)
+    def testSwiftClientUploadCmdFail(self):
+        self.mock = flexmock()
+        flexmock(subprocess.Popen).new_instances(self.mock)
+        self.mock.should_receive('wait').and_return(True)
+        self.mock.returncode = '0'
+        self.mock.should_receive('communicate').and_raise(SwiftCommandError())
+        swiftClient = SwiftClient()
+        swiftClient.upload('config', 'aaaaa')
+
     def testSwiftClientNormalCase(self):
+        self.mock = flexmock()
+        flexmock(subprocess.Popen).new_instances(self.mock)
+        self.mock.should_receive('wait').and_return(True)
+        self.mock.returncode = '0'
+        self.mock.should_receive('communicate').and_return(["etc/network/interfaces\n", None])
         swiftClient = SwiftClient()
         nose.tools.ok_(swiftClient.upload('config', '/etc/network/interfaces'))
+        self.mock.returncode = '0'
+        self.mock.should_receive('communicate').and_return(["etc/network/interfaces\n", None])
         nose.tools.ok_(swiftClient.delete('config', '/etc/network/interfaces'))
 
+    def testBackupToCloudBackup(self):
+        self.mock = flexmock()
+        flexmock(subprocess.Popen).new_instances(self.mock)
+        self.mock.should_receive('wait').and_return(True)
+        mock = flexmock()
+        mock.should_receive('upload').and_return(True)
+        backupConf = BackupToCloud(None, mock)
+        datetime = int(backupConf.backup())
+        now = int(strftime("%Y%m%d%H%M", gmtime()))
+        if now > datetime:
+            nose.tools.ok_((now == datetime + 1), "now(%d) > datetime(%d) + 1" % (now, datetime))
+        else:
+            nose.tools.ok_((now == datetime), "now(%d) != datetime(%d)" % (now, datetime))
+
     def testGetConfNormalCase(self):
+        
         result = json.loads(save_gateway_configuration())
         info = json.loads(get_configuration_backup_info())
         print info
