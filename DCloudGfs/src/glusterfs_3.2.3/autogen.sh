fac29d75f108fc98cf1a3ee0ce819c757cedc7b0
ken.liu <forken.liu@delta.com.tw>
2012-07-12 13:41:57 +0800

    Remove DCloudGfs

--- DCloudGfs/src/glusterfs_3.2.3/autogen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-aclocal
-autoheader
-(libtoolize --automake --copy --force || glibtoolize --automake --copy --force)
-autoconf
-automake --add-missing --copy --foreign
-cd argp-standalone;./autogen.sh
