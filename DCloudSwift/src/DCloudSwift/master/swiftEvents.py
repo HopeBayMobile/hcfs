c8125def4e6ddaec190f00f24d00e82765bd8efc
Ken Liu <forken.liu@delta.com.tw>
2012-07-20 16:01:53 +0800

    modify event definition

--- DCloudSwift/src/DCloudSwift/master/swiftEvents.py
+++ DCloudSwift/src/DCloudSwift/master/swiftEvents.py
@@ -1,14 +1,11 @@
-def enum(**enums):
-    return type('Enum', (), enums)
-
 class HDD:
     event_name = "HDD"
-    level_name = enum(OK="OK", WARNING="WARNING", ERROR="ERROR")
-    healthy = enum(YES=True, NO=False)
+    level_name = ["OK", "WARNING", "ERROR"]
+    healthy = [True, False]
 
 class HEARTBEAT:
     event_name = "HEARTBEAT"
     level_name = None
-    status = enum(ALIVE="alive", UNKNOWN="unknown", DEAD="dead")
+    status = ["alive", "unknown", "dead"]
 
 EVENT_LIST = [HDD, HEARTBEAT]
