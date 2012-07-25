04f6d6c8b336d4f838b6786e118db699eff82604
andy24 <andy.hung@delta.com.tw>
2012-07-25 18:40:31 +0800

    add MaintainReport and fix switcher bug

--- DCloudSwift/src/DCloudSwift/master/SwiftMaintainSwitcher.py
+++ DCloudSwift/src/DCloudSwift/master/SwiftMaintainSwitcher.py
@@ -13,17 +13,20 @@ import pickle
 import signal
 import simplejson as json
 
+WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
+BASEDIR = os.path.dirname(os.path.dirname(WORKING_DIR))
+sys.path.append("%s/DCloudSwift/" % BASEDIR)
+
 from util.daemon import Daemon
 from util.util import GlobalVar
 from util.SwiftCfg import SwiftMasterCfg
 from util import util
+from util.database import NodeInfoDatabaseBroker
 
 from common.events import HDD
 from common.events import HEARTBEAT
 
-WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
-BASEDIR = os.path.dirname(os.path.dirname(WORKING_DIR))
-sys.path.append("%s/DCloudSwift/" % BASEDIR)
+
 
 
 class SwiftMaintainSwitcher(Daemon):
@@ -34,33 +37,45 @@ class SwiftMaintainSwitcher(Daemon):
     Those nodes or disks will add or modify on maintain table.
     """
 
-    def __init__(self, pidfile, replicationTime=None,
+    def __init__(self, pidfile, DBFile=None, replicationTime=None,
                  refreshTime=None, daemonSleep=None):
         """
         construct SwiftMaintainSwitcher daemon and create pid file
         it will read replication_time from swift_master.ini
         @type pidfile: string
         @param pidfile: pid file path
+        @type DBFile: string
+        @param DBFile: sqlite db file abstract path
+        @type replicationTime: integer
+        @param replicationTime: waiting time for replication
+        @type refreshTime: integer
+        @param refreshTime: after refresh time, we will claim a node is missing.
+        @type daemonSleep: integer
+        @param daemonSleep: the daemon sleep time
         """
         Daemon.__init__(self, pidfile)
         logger = util.getLogger(name='swiftmaintainswitcher')
         self.masterCfg = SwiftMasterCfg(GlobalVar.MASTERCONF)
+        if DBFile is None:
+            self.DBFile = SwiftMasterCfg(GlobalVar.NODE_DB)
+        else:
+            self.DBFile = DBFile
         if replicationTime is None:
-            self.replicationTime = self.masterCfg \
-                .getKwparams()['maintainReplTime']
+            self.replicationTime = int(self.masterCfg \
+                .getKwparams()['maintainReplTime'])
         else:
             self.replicationTime = replicationTime
         if refreshTime is None:
-            self.refreshTime = self.masterCfg \
-                .getKwparams()['maintainRefreshTime']
+            self.refreshTime = int(self.masterCfg \
+                .getKwparams()['maintainRefreshTime'])
         else:
             self.refreshTime = refreshTime
         if daemonSleep is None:
-            self.daemonSleep = self.masterCfg \
-                .getKwparams()['maintainDaemonSleep']
+            self.daemonSleep = int(self.masterCfg \
+                .getKwparams()['maintainDaemonSleep'])
         else:
             self.daemonSleep = daemonSleep
-        self.db = NodeInfoDatabaseBroker()
+        self.db = NodeInfoDatabaseBroker(self.DBFile)
 
     def checkService(self):
         """
@@ -68,29 +83,36 @@ class SwiftMaintainSwitcher(Daemon):
         """
         logger = util.getLogger(name='swiftmaintainswitcher.checkService')
         self.nowtimeStamp = int(time.mktime(time.localtime()))
-        serviceNode = self.db.show_node_info_table()
+        logger.info("start check hostname which is in service mode")
+        serviceNode = self.db.query_node_info_table('mode = "service"').fetchall()
         for row in serviceNode:
-            print row
             if row[1] == HEARTBEAT.status[2]:
-                self.updateStatusWaiting(row[0])
+                record = self.updateStatusWaiting(row[0])
+                logger.info("the record which update status to waiting: %s"
+                            % str(self.dict_from_row(record)))
                 continue
             if row[1] == HEARTBEAT.status[0]:
                 diskInfo = json.loads(row[3])
                 if ((self.refreshTime + row[2]) < self.nowtimeStamp):
-                    self.updateStatusWaiting(row[0])
+                    record = self.updateStatusWaiting(row[0])
+                    logger.info("the record which update status to waiting: %s"
+                            % str(self.dict_from_row(record)))
                     continue
                 if (('missing' in diskInfo) or ('broken' in diskInfo)):
                     if (diskInfo['timestamp']
                             + self.replicationTime > self.nowtimeStamp):
-                        self.updateStatusWaiting(row[0])
+                        record = self.updateStatusWaiting(row[0])
+                        logger.info("the record which update status to waiting: %s"
+                                    % str(self.dict_from_row(record)))
                         continue
+        logger.info("end check hostname which is in service mode")
 
     def updateStatusService(self, hostname=None):
         """
         update node status from waiting to service
         @rtype: boolean
         @return: Return False if update fail.
-                 Return True if update success.
+                 Return raw data if update success.
         """
         return self.updateStatus(hostname, 'service')
 
@@ -99,7 +121,7 @@ class SwiftMaintainSwitcher(Daemon):
         update node status from service to waiting
         @rtype: boolean
         @return: Return False if update fail.
-                 Return True if update success.
+                 Return raw data if update success.
         """
         return self.updateStatus(hostname, 'waiting')
 
@@ -108,44 +130,57 @@ class SwiftMaintainSwitcher(Daemon):
         can update status by hostname
         @rtype: boolean
         @return: Return False if hostname is None.
-                 Return True if update success.
+                 Return raw data if update success.
         """
         if hostname is None:
             return False
         try:
-            db.update_node_mode(hostname, status, self.nowtimeStamp)
-            return True
+            return db.update_node_mode(hostname, status, self.nowtimeStamp)
         except sqlite3.Error, e:
             logger.error('sqlite update error: %s' % e.args[0])
             return False
 
     def checkWaiting(self):
+        """
+        implement status change logic for waiting mode to service mode
+        """
         logger = util.getLogger(name='swiftmaintainswitcher.checkWaiting')
         self.nowtimeStamp = int(time.mktime(time.localtime()))
-        waitingNode = self.db.show_node_info_table()
+        logger.info("start check hostname which is in waiting mode")
+        waitingNode = self.db.query_node_info_table('mode = "waiting"').fetchall()
         for row in waitingNode:
-            print row
             if row[1] == HEARTBEAT.status[0]:
                 diskInfo = json.loads(row[3])
                 if (('missing' not in diskInfo) or ('broken' not in diskInfo)):
-                    self.updateStatusService(row[0])
+                    record = self.updateStatusService(row[0])
+                    logger.info("the record which update status to waiting: %s"
+                                    % str(self.dict_from_row(record)))
                     continue
+        logger.info("end check hostname which is in waiting mode")
+
+    def dict_from_row(self, row):
+        dt = {}
+        for clName in row.keys():
+            dt[clName] = row[clName]
+        return dt 
 
     def run(self):
         """
         start daemon and check service and waiting mode
         """
         logger = util.getLogger(name='swiftmaintainswitcher.run')
-        while (True):
+        while True:
+            logger.error("daemon sleep: %s" % self.daemonSleep)
             self.checkService()
             self.checkWaiting()
-            time.sleep(self.daemonSleep)
+            time.sleep(int(self.daemonSleep))
 
 
-def main():
-    daemon = SwiftMaintainSwitcher('/var/run/SwiftMaintainSwitcher.pid')
+def main(DBFile=None):
+    daemon = SwiftMaintainSwitcher('/var/run/SwiftMaintainSwitcher.pid', DBFile)
     if len(sys.argv) == 2:
         if 'start' == sys.argv[1]:
+            print "daemon start"
             daemon.start()
         elif 'stop' == sys.argv[1]:
             daemon.stop()
@@ -160,4 +195,31 @@ def main():
         sys.exit(2)
 
 if __name__ == "__main__":
-    main()
+    DBFile = '/etc/test/test.db'
+    os.system("rm -f %s" % DBFile)
+    db = NodeInfoDatabaseBroker(DBFile)
+    db.initialize()
+    timestamp = int(time.mktime(time.localtime()))
+    diskInfo = {
+                    'timestamp': timestamp,
+                    'missing': {
+                                    'count': 6,
+                                    'timestamp': timestamp,
+                                },
+                    'broken': [
+                               {
+                                    'SN': 'aaaaa',
+                                    'timestamp': timestamp,
+                               },
+                              ],
+                    'healthy': [
+                                {
+                                    'SN': 'aaaaa',
+                                    'timestamp': timestamp,
+                                }
+                               ],
+                }
+    row = db.add_node('192.168.1.20', 'dead', timestamp-100, json.dumps(diskInfo), 'service', timestamp)
+    row = db.add_node('192.168.1.100', 'alive', timestamp-100, '{}', 'waiting', timestamp)
+    main(DBFile)
+    
---
+++ DCloudSwift/src/DCloudSwift/util/MaintainReport.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python -u
+# -*- coding: utf-8 -*-
+
+# Developed by Cloud Data Team, Cloud Technology Center, Delta Electronic Inc.
+
+import os
+import sys
+import simplejson as json
+
+WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
+BASEDIR = os.path.dirname(os.path.dirname(WORKING_DIR))
+sys.path.append("%s/DCloudSwift/" % BASEDIR)
+
+from util import *
+from daemon import Daemon
+from util import GlobalVar
+from SwiftCfg import SwiftMasterCfg
+from database import MaintenanceBacklogDatabaseBroker
+
+
+
+class MaintainReport():
+    
+    def __init__(self, DBFile=None):
+        self.report = {}
+        if DBFile is None:
+            self.DBFile = SwiftMasterCfg(GlobalVar.NODE_DB)
+        else:
+            self.DBFile = DBFile
+        self.db = MaintenanceBacklogDatabaseBroker(self.DBFile)
+
+    def query(self, target = None):
+        result = None
+        if target is None:
+            result = self.db.show_maintenance_backlog_table()
+        else:
+            result = self.db.query_maintenance_backlog_table('target = %s' % target)
+        for row in result:
+            print row
+
+def main(DBFile=None):
+    maintainReport = MaintainReport(DBFile)
+    maintainReport.query()
+
+if __name__ == '__main__':
+    DBFile = None
+    if len(sys.argv) == 2:
+        if 'test' == sys.argv[1]:
+            DBFile = '/etc/test/test.db'
+            os.system("rm -f %s" % DBFile)
+            db = MaintenanceBacklogDatabaseBroker(DBFile)
+            db.initialize()
+            timestamp = int(time.mktime(time.localtime()))
+            diskReserve = ['SN_reserve_001', 'SN_reserve_002', 'SN_reserve_003']
+            diskReplace = ['SN_replace001', 'SN_replace002', 'SN_replace003']
+            row = db.add_maintenance_backlog('node_missing', '192.168.2.30', json.dumps(diskReserve), json.dumps(diskReplace), timestamp)
+    main(DBFile)
