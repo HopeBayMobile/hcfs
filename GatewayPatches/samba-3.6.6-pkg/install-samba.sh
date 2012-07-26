d4a6af5b874502f398b4a3217648f5f0b7b5f7a3
wthung <weitang.hung@delta.com.tw>
2012-07-26 12:58:54 +0800

    add install scripts

---
+++ GatewayPatches/samba-3.6.6-pkg/install-samba.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# need to automate the selection of configuration
+dpkg -i libattr1_2.4.46-8ubuntu1_amd64.deb
+dpkg -i libattr1-dev_2.4.46-8ubuntu1_amd64.deb
+dpkg -i libacl1_2.2.51-8ubuntu1_amd64.deb
+dpkg -i libwbclient0_3.6.6-2ubuntu1_amd64.deb
+dpkg -i samba-common_3.6.6-2ubuntu1_all.debY
+dpkg -i samba_3.6.6-2ubuntu1_amd64.deb
+dpkg -i smbclient_3.6.6-2ubuntu1_amd64.deb
+dpkg -i winbind_3.6.6-2ubuntu1_amd64.deb
+dpkg -i libpam-smbpass_3.6.6-2ubuntu1_amd64.deb
+dpkg -i libpam-winbind_3.6.6-2ubuntu1_amd64.deb
+
---
+++ GatewayPatches/ubuntu1204/install-u1204.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dpkg -i linux-image-3.2.19-gateway_3.2.19-gateway-10.00.Custom_amd64.deb
+dpkg -i linux-headers-3.2.19-gateway_3.2.19-gateway-10.00.Custom_amd64.deb
+dpkg -i libfuse2_2.8.6-2ubuntu2_amd64.deb
+dpkg -i fuse_2.8.6-2ubuntu2_amd64.deb
+dpkg -i fuse-utils_2.8.6-2ubuntu2_all.deb
+
