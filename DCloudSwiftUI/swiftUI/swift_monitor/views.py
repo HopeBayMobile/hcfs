cf940053f02c0b2a36e54decdeed36041773b1bb
jsleetw <ail529@gmail.com>
2012-08-17 18:02:21 +0800

    implement onclick open/close sub tab

--- DCloudSwiftUI/swiftUI/swift_monitor/views.py
+++ DCloudSwiftUI/swiftUI/swift_monitor/views.py
@@ -9,13 +9,13 @@ def index(request):
     """
     zone = {"ip":"192.168.1.104","nodes":3,"used":"21","free":"79","capacity":"12TB"}
     nodes = []
-    nodes.append({"ip":"172.30.11.33","hostname":"TPEIIA","status":"dead","mode":"waiting","hd_number":6,"hd_error":1,
+    nodes.append({"index":"1","ip":"172.30.11.33","hostname":"TPEIIA","status":"dead","mode":"waiting","hd_number":6,"hd_error":1,
     "hd_info":[{"serial":"SN_TP02","status":"Broken"},{"serial":"SN_TP03","status":"OK"},{"serial":"SN_TP04","status":"OK"}]
     })
-    nodes.append({"ip":"172.30.11.37","hostname":"TPEIIB","status":"alive","mode":"waiting","hd_number":6,"hd_error":0,
+    nodes.append({"index":"2", "ip":"172.30.11.37","hostname":"TPEIIB","status":"alive","mode":"waiting","hd_number":6,"hd_error":0,
     "hd_info":[{"serial":"SN_TP02","status":"OK"},{"serial":"SN_TP03","status":"OK"},{"serial":"SN_TP04","status":"OK"}]
     })
-    nodes.append({"ip":"172.30.11.25","hostname":"TPEIIC","status":"alive","mode":"service","hd_number":6,"hd_error":0,
+    nodes.append({"index":"3", "ip":"172.30.11.25","hostname":"TPEIIC","status":"alive","mode":"service","hd_number":6,"hd_error":0,
     "hd_info":[{"serial":"SN_TP02","status":"OK"},{"serial":"SN_TP03","status":"OK"},{"serial":"SN_TP04","status":"OK"}]
     })
     return render_to_response('monitor.html', {"request": request, "zone":zone,"nodes":nodes})
