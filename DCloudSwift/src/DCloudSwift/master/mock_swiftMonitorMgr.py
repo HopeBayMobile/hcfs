35ee45ec0b7c422bf9fafa9b52c08594637928a4
gasolin <gasolin@gmail.com>
2012-08-22 09:40:52 +0800

    update mockup SwiftMonitorMgr to support open/close sub tree

--- DCloudSwift/src/DCloudSwift/master/mock_swiftMonitorMgr.py
+++ DCloudSwift/src/DCloudSwift/master/mock_swiftMonitorMgr.py
@@ -20,13 +20,13 @@ class SwiftMonitorMgr:
         Get all nodes infomations
         """
         nodes_info = []
-        nodes_info.append({"ip":"172.30.11.33","hostname":"TPEIIA","status":"dead","mode":"waiting","hd_number":6,"hd_error":1,
+        nodes_info.append({"ip":"172.30.11.33","index":"1","hostname":"TPEIIA","status":"dead","mode":"waiting","hd_number":6,"hd_error":1,
         "hd_info":[{"serial":"SN_TP02","status":"Broken"},{"serial":"SN_TP03","status":"OK"},{"serial":"SN_TP04","status":"OK"}]
         })
-        nodes_info.append({"ip":"172.30.11.37","hostname":"TPEIIB","status":"alive","mode":"waiting","hd_number":6,"hd_error":0,
+        nodes_info.append({"ip":"172.30.11.37","index":"2","hostname":"TPEIIB","status":"alive","mode":"waiting","hd_number":6,"hd_error":0,
         "hd_info":[{"serial":"SN_TP02","status":"OK"},{"serial":"SN_TP03","status":"OK"},{"serial":"SN_TP04","status":"OK"}]
         })
-        nodes_info.append({"ip":"172.30.11.25","hostname":"TPEIIC","status":"alive","mode":"service","hd_number":6,"hd_error":0,
+        nodes_info.append({"ip":"172.30.11.25","index":"3","hostname":"TPEIIC","status":"alive","mode":"service","hd_number":6,"hd_error":0,
         "hd_info":[{"serial":"SN_TP02","status":"OK"},{"serial":"SN_TP03","status":"OK"},{"serial":"SN_TP04","status":"OK"}]
         })
         return nodes_info
