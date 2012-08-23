b7ea69d185a95ee45f546e02d570e1f68b5d450c
gasolin <gasolin@gmail.com>
2012-08-23 14:57:50 +0800

    show top usage instead of quota, with human readable format

--- DCloudSwiftUI/swiftUI/swift_dashboard/views.py
+++ DCloudSwiftUI/swiftUI/swift_dashboard/views.py
@@ -20,10 +20,19 @@ def index(request):
         account_list = []
         for val in accounts:
             if(accounts[val]["account_enable"]):
-                current_quota = int(accounts[val]["quota"])
-                account_list.append(dict(id=val,quota=current_quota))
+                current_usage = int(accounts[val]["usage"])
+                account_list.append(dict(id=val,usage=current_usage))
         #sort list to get top 5
-        top_list = sorted(account_list, key=lambda k: k['quota'])[::-1][:5]
+        top_list = sorted(account_list, key=lambda k: k['usage'])[::-1][:5]
+        for i in top_list:
+            human_int = i["usage"]/1000000
+            human_float = (i["usage"] - (human_int*1000000))/10000
+            if(human_float<10):
+                append_float="0"+str(human_float)
+            else:
+                append_float=str(human_float)
+            human_read = str(human_int) + "." + append_float +"G"
+            i["husage"] = human_read
         return render_to_response('dashboard.html', {"request": request,"zone":zone,"accounts": top_list})
     else:
         return HttpResponse("something wrong in list_account:")
