c836b6c96a705b09f75324927b6e1e2ed5742bf6
Ken Liu <forken.liu@delta.com.tw>
2012-08-24 18:41:19 +0800

    Implement list_nodes_info

--- DCloudSwift/src/DCloudSwift/master/swiftMonitorMgr.py
+++ DCloudSwift/src/DCloudSwift/master/swiftMonitorMgr.py
@@ -11,7 +11,7 @@ from ConfigParser import ConfigParser
 
 WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
 BASEDIR = os.path.dirname(os.path.dirname(WORKING_DIR))
-sys.path.insert(0,"%s/DCloudSwift/" % BASEDIR)
+sys.path.insert(0, "%s/DCloudSwift/" % BASEDIR)
 
 from util.SwiftCfg import SwiftMasterCfg
 from util.util import GlobalVar
@@ -117,8 +117,58 @@ class SwiftMonitorMgr:
         
         return percentage
 
+    def get_hd_error(self, disk_info_json):
+        '''
+        calculate ddfree capacity
+        @param total: total capacity
+        @param used: used capacity
+        @param unusable: unusable capacity due to broken nodes or disks 
+        '''
+        disk_info = json.loads(disk_info_json)
+
+        error = 0
+        missing = disk_info.get("missing", {})
+        error += missing.get("count", 0)
+
+        for hd in disk_info.get("broken", []):
+            error += 1
+        
+        return error
+
+    def get_hd_info(self, disk_info_json):
+        '''
+        calculate ddfree capacity
+        @param total: total capacity
+        @param used: used capacity
+        @param unusable: unusable capacity due to broken nodes or disks 
+        '''
+        disk_info = json.loads(disk_info_json)
+
+        hd_info = []
+        for hd in disk_info.get("healthy", []):
+            hd_info.append({"serial": hd.get("SN", "N/A"), "status": "OK"})
+
+        for hd in disk_info.get("broken", []):
+            hd_info.append({"serial": hd.get("SN", "N/A"), "status": "Broken"})
+        
+        return hd_info
+
     def get_portal_url(self):
         return util.getPortalUrl()
+
+    def get_private_ip(self, hostname, nameserver="192.168.11.1"):
+        '''
+        lookup ip addresses of hosts.
+
+        @param  hostname: hostname to lookup ip
+        @param nameserver: ip address of the nameserver, and the default
+            value is 192.168.11.1
+        @return: private ip of hostname
+        '''
+
+        ip = util.hostname2Ip(hostname=hostname, nameserver=nameserver)  # This function returns None if lookup failed
+
+        return ip
 
     def get_zone_info(self):
         """
@@ -145,7 +195,7 @@ class SwiftMonitorMgr:
                 used = "%.2f" % (self.calculate_used_capacity_percentage(total_capacity, used_capacity))
                 free = "%.2f" % (self.calculate_free_capacity_percentage(total_capacity, used_capacity))
             
-        zone = {"url": url, "nodes": nodes, "used": used, "free": free, "capacity": capacity}
+        zone = {"ip": url, "nodes": nodes, "used": used, "free": free, "capacity": capacity}
         
         return zone
 
@@ -167,17 +217,41 @@ class SwiftMonitorMgr:
         
         """
         nodes_info = []
-        nodes_info.append({"ip":"172.30.11.33","index":"1","hostname":"TPEIIA","status":"dead","mode":"waiting","hd_number":6,"hd_error":1,
-        "hd_info":[{"serial":"SN_TP02","status":"Broken"},{"serial":"SN_TP03","status":"OK"},{"serial":"SN_TP04","status":"OK"}]
-        })
-        nodes_info.append({"ip":"172.30.11.37","index":"2","hostname":"TPEIIB","status":"alive","mode":"waiting","hd_number":6,"hd_error":0,
-        "hd_info":[{"serial":"SN_TP02","status":"OK"},{"serial":"SN_TP03","status":"OK"},{"serial":"SN_TP04","status":"OK"}]
-        })
-        nodes_info.append({"ip":"172.30.11.25","index":"3","hostname":"TPEIIC","status":"alive","mode":"service","hd_number":6,"hd_error":0,
-        "hd_info":[{"serial":"SN_TP02","status":"OK"},{"serial":"SN_TP03","status":"OK"},{"serial":"SN_TP04","status":"OK"}]
-        })
+        db = NodeInfoDatabaseBroker(util.GlobalVar.NODE_DB)
+        node_info_table = db.show_node_info_table()
+
+        if not node_info_table:
+            return nodes_info
+
+        index = 0
+        for info in node_info_table:
+            index += 1
+            disk_info_json = info["disk"]
+            spec = db.get_spec(info["hostname"])
+            if not spec:
+                self.logger.error("Failed to get spec of %s" % info["hostname"])
+            
+            hostname = info["hostname"]
+            ip = self.get_private_ip(hostname=hostname)
+            status = info["status"]
+            mode = info["mode"]
+            hd_number = spec["diskcount"] if spec else 0
+            hd_error = self.get_hd_error(disk_info_json)
+            hd_info = self.get_hd_info(disk_info_json)
+
+            nodes_info.append({"hostname": hostname,
+                               "index": str(index), 
+                               "ip": ip,
+                               "status": status, 
+                               "mode": mode, 
+                               "hd_number": hd_number,
+                               "hd_error": hd_error,
+                               "hd_info": hd_info,
+                              })
+
         return nodes_info
 
 if __name__ == '__main__':
     SM = SwiftMonitorMgr()
-    print SM.get_zone_info()
+    #print SM.get_zone_info()
+    print SM.list_nodes_info()
