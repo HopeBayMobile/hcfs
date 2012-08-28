c3cd873df7961527fa7cde38ccf7b350177c6994
gasolin <gasolin@gmail.com>
2012-08-28 11:36:21 +0800

    handle default swift account Error for not showing usage

--- DCloudSwiftUI/swiftUI/swift_dashboard/views.py
+++ DCloudSwiftUI/swiftUI/swift_dashboard/views.py
@@ -24,8 +24,10 @@ def index(request):
     SM = SwiftMonitorMgr()
     zone = SM.get_zone_info()
     
-    total_capacity = 100
-    used_capacity = 0
+    #logical maximum capacity
+    #total_capacity = 100
+    #current usage for all accounts
+    #used_capacity = 0
     
     SA = SwiftAccountMgr()
     result = SA.list_account()
@@ -33,29 +35,20 @@ def index(request):
         accounts = result.msg
         account_list = []
         for val in accounts:
-            if(accounts[val]["account_enable"]):
+            if(accounts[val]["account_enable"] && accounts[val]["usage"]!="Error"):
                 current_usage = int(accounts[val]["usage"])
-                current_quota = int(accounts[val]["quota"])
-                used_capacity+=current_usage
-                total_capacity+=current_quota
+                #current_quota = int(accounts[val]["quota"])
+                #used_capacity+=current_usage
+                #total_capacity+=current_quota
                 account_list.append(dict(id=val,usage=current_usage))
         #sort list to get top 5
         top_list = sorted(account_list, key=itemgetter('usage'))[::-1][:5]
         for i in top_list:
-            #make data readable
-            #human_int = i["usage"]/1000000
-            #human_float = (i["usage"] - (human_int*1000000))/10000
-            #if(human_float<10):
-            #    append_float="0"+str(human_float)
-            #else:
-            #    append_float=str(human_float)
-            #human_read = str(human_int) + "." + append_float +"G"
-            #i["husage"] = human_read
             i["husage"] = human_readable_capacity(i["usage"])
-        used_ratio = (used_capacity*100)/total_capacity
-        zone["used"] = used_ratio
-        zone["free"] = 100-used_ratio
-        zone["quota"] = human_readable_capacity(total_capacity)
+        #used_ratio = (used_capacity*100)/total_capacity
+        #zone["used"] = used_ratio
+        #zone["free"] = 100-used_ratio
+        #zone["quota"] = human_readable_capacity(total_capacity)
         return render_to_response('dashboard.html', {"request": request,"zone":zone,"accounts": top_list})
     else:
         return HttpResponse("something wrong in list_account:")
