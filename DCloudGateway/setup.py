48c84b9cd2ddd1f81b4c0f93138dadc61e1aa15f
unknown <JASHING.HUANG@TWTP2NB0566.delta.corp>
2012-09-04 16:46:35 +0800

    place add_HDD_to_MD.py at rc.local

--- DCloudGateway/setup.py
+++ DCloudGateway/setup.py
@@ -87,6 +87,8 @@ def main():
         os.system("chmod -R 777 /root")
         os.system("chown -R www-data:www-data %s"%CONFDIR)
         os.system("cp -rs /usr/bin/*s3ql* /usr/local/bin")
+        os.system("echo \"python /etc/delta/add_HDD_to_MD.py\" > /etc/rc.local ")
+        
 
 if __name__ == '__main__':
     main()
