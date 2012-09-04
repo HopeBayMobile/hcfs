1eb253cd8813be5b1d55ccbbfd0a48f131a17285
swift <swift@swift-VM.(none)>
2012-09-04 10:59:32 +0800

    add deploy_module command for module testing

--- DCloudSwiftUI/fabfile.py
+++ DCloudSwiftUI/fabfile.py
@@ -1,11 +1,24 @@
 """
 web deploy script
-0.1 fredlin 2011/9/3
+0.2 fredlin 2011/9/4
+
+First time usage:
+
+In linux machine, run command in console:
+
+$ fab setup
+$ fab vncserver
+
+then open another console to run
+
+$ fab vnc
+
 """
 from fabric.api import *
 
 #modify local repository path
-src_path = "/home/swift/StorageAppliance/DCloudSwiftUI/swiftUI/"
+src_path = "~/StorageAppliance/DCloudSwiftUI/swiftUI/"
+module_path = "~/StorageAppliance/DCloudSwift/src/DCloudSwift/"
 
 env.hosts=["10.1.0.17"]
 env.user="root"
@@ -22,6 +35,9 @@ def vncserver():
 def vnc():
     #start vncviewer on local machine
     local("vncviewer -via root@"+env.hosts[0]+" :1")
+
+def deploy_module():
+    local("scp -r "+module_path+" root@10.1.0.17:/templateVol/2/rw/TPE1AA0122/usr/local/lib/python2.7/dist-packages/DCloudSwift-0.5-py2.7.egg/")
 
 def deploy():
     # deploy local web directory to target server
