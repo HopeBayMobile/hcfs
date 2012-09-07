5ffd7f5394af60ee01e13df8206eb50d932eaac1
yenkuo <yen.kuo@delta.com.tw>
2012-09-07 11:42:40 +0800

    add comments to install_gateway_all_in_one.sh

--- INSTALL_Gateway/install_gateway_all_in_one.sh
+++ INSTALL_Gateway/install_gateway_all_in_one.sh
@@ -23,11 +23,13 @@ if [ "$1" != "dom" ]; then
 fi
 
 # install gateway API
+    echo "        ***** apt-get install dcloud-gateway *****"
     apt-get install -y --force-yes dcloud-gateway
     check_ok
     apt-get -y --force-yes -f install
 
 # 1. append apt-server domain name
+echo "        ***** append APT-server to /etc/apt/sources.list.d/ *****"
 cat >/etc/apt/sources.list.d/delta-server-precise.list <<EOF
 deb http://apt.delcloudia.com/packages/ubuntu/ precise main
 deb-src http://apt.delcloudia.com/packages/ubuntu precise main
@@ -37,21 +39,24 @@ EOF
     # 2. sudo apt-key adv --recv-keys ...
 
 # FIXME - Clean up unsed files to free up space
+echo "        ***** clean up files *****"
 rm -r /usr/share/doc /usr/src
 rm -r /usr/local/MYSQL/mysql-test	## just a test, not sure whether it is safe.
 
 # install samba 3.6.6
+echo "        ***** Install Samba patches *****"
 cd ../GatewayPatches
 ./install-samba.sh
 # install kernel and fuse patches
+echo "        ***** Install kernel and fuse patches *****"
 ./install-u1204.sh
 
 # install gateway GUI
+echo "        ***** Install gateway GUI *****"
 apt-get -y --force-yes -f install
 cd ../DCloudGatewayUI
 ./setup.sh
 update-rc.d celeryd defaults
 
-
-sleep 3
+sleep 1
 echo "Installation of Gateway is completed..."
