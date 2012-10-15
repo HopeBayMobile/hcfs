a3f02f49c24fe8b4fb744dd1c1237f9c35354a74
yenkuo <yen.kuo@delta.com.tw>
2012-10-15 15:32:18 +0800

    start writing super build.

---
+++ CI_scripts/super_build/super_build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -x
+echo "************************"
+echo "By calling this script, it will (a) git pull, (b) run StorageAppliance/CI_scripts/build_scripts/build_gw_package.sh"
+echo "Usage: ./super_build.sh <git_branch> <build_num>"
+echo "************************"
