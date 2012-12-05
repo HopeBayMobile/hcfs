587df68a6eaf1d74c5b427cb9febf2da31a00d53
fangyuxun <fangyuxun@delta.com.tw>
2012-12-05 16:05:08 +0800

    remove stdout log handler in fsck

--- DCloudS3ql/src/s3ql/fsck.py
+++ DCloudS3ql/src/s3ql/fsck.py
@@ -1182,7 +1182,10 @@ def main(args=None):
         args = sys.argv[1:]
 
     options = parse_args(args)
-    setup_logging(options)
+    stdout_log_handler = gitsetup_logging(options)
+    
+    if stdout_log_handler:
+        logging.getLogger().removeHandler(stdout_log_handler)
 
     # Check if fs is mounted on this computer
     # This is not foolproof but should prevent common mistakes
