3ebf61888bc474510f5f928629fa135753ceb1ea
weitang.hung <weitang.hung@delta.com.tw>
2012-12-18 14:50:24 +0800

    1. remove the installation of samba/winbind related packages 2. update Install_ldap_samba.sh 3. backup Install_ldap_samba.sh to Install_ldap_samba.sh.old

--- GatewayPatches/install-samba.sh
+++ GatewayPatches/install-samba.sh
@@ -12,12 +12,12 @@ cd debsrc/
 dpkg -i libattr1_2.4.46-8ubuntu1_amd64.deb
 dpkg -i libattr1-dev_2.4.46-8ubuntu1_amd64.deb
 dpkg -i libacl1_2.2.51-8ubuntu1_amd64.deb
-dpkg -i libwbclient0_3.6.6-2ubuntu1_amd64.deb
-dpkg -i samba-common_3.6.6-2ubuntu1_all.deb
-dpkg -i samba_3.6.6-2ubuntu1_amd64.deb
-dpkg -i smbclient_3.6.6-2ubuntu1_amd64.deb
-dpkg -i winbind_3.6.6-2ubuntu1_amd64.deb
-dpkg -i libpam-smbpass_3.6.6-2ubuntu1_amd64.deb
-dpkg -i libpam-winbind_3.6.6-2ubuntu1_amd64.deb
+#dpkg -i libwbclient0_3.6.6-2ubuntu1_amd64.deb
+#dpkg -i samba-common_3.6.6-2ubuntu1_all.deb
+#dpkg -i samba_3.6.6-2ubuntu1_amd64.deb
+#dpkg -i smbclient_3.6.6-2ubuntu1_amd64.deb
+#dpkg -i winbind_3.6.6-2ubuntu1_amd64.deb
+#dpkg -i libpam-smbpass_3.6.6-2ubuntu1_amd64.deb
+#dpkg -i libpam-winbind_3.6.6-2ubuntu1_amd64.deb
 
 cd ..
