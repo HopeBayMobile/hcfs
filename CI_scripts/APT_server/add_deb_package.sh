b38b0a7f50a29d4557e9807f57761723bc7aa0ee
fangyuxun <fangyuxun@delta.com.tw>
2013-03-13 10:16:27 +0800

    Update add deb to atp-server script, Now would remove old packages before import newest packages

--- CI_scripts/APT_server/add_deb_package.sh
+++ CI_scripts/APT_server/add_deb_package.sh
@@ -3,5 +3,8 @@
 ## Can be ran on APT-server
 
 cd /var/packages/ubuntu
+# First remvoe old packages
+sudo reprepro remove precise s3ql s3ql-dbg savebox dcloud-gateway dcloudgatewayapi
+# Then put newest packages 
 sudo reprepro includedeb precise ~/debsrc/*.deb
 rm ~/debsrc/*
