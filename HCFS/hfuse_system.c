839f1f2f65ead59557394d7b55dab47611281cb4
jiahongwu <jiahong.wu@delta.com.tw>
2013-11-13 18:42:35 +0800

    New helper routines and fuse functions

---
+++ HCFS/hfuse_system.c
@@ -0,0 +1,70 @@
+#include "fuseop.h"
+#include "super_inode.h"
+#include "dir_lookup.h"
+#include <fuse.h>
+
+void init_hfuse()
+ {
+  int ret_val;
+  char rootmetapath[400];
+  ino_t root_inode;
+  struct stat this_stat;
+  DIR_META_TYPE this_meta;
+  DIR_ENTRY_PAGE temppage;
+  mode_t self_mode;
+  FILE *metafptr;
+
+  ret_val = super_inode_init();
+  init_pathname_cache();
+
+  /* Check if need to initialize the root meta file */
+  fetch_meta_path(rootmetapath,1);
+  if (access(rootmetapath,F_OK)!=0)
+   {
+    memset(&this_stat,0,sizeof(struct stat));
+    memset(&this_meta,0,sizeof(DIR_META_TYPE));
+    memset(&temppage,0,sizeof(DIR_ENTRY_PAGE));
+
+    self_mode = S_IFDIR | 0755;
+    this_stat.st_mode = self_mode;
+    this_stat.st_nlink = 2;   /*One pointed by the parent, another by self*/
+    this_stat.st_uid = getuid();
+    this_stat.st_gid = getgid();
+    this_stat.st_atime = time(NULL);
+    this_stat.st_mtime = this_stat.st_atime;
+    this_stat.st_ctime = this_stat.st_ctime;
+
+    root_inode = super_inode_new_inode(&this_stat);
+    /*TODO: put error handling here if root_inode is not 1 (cannot initialize system)*/
+
+    this_stat.st_ino = 1;
+    memcpy(&(this_meta.thisstat),&this_stat,sizeof(struct stat));
+
+    metafptr = fopen(rootmetapath,"w");
+
+    ret_val = fwrite(&this_meta,sizeof(DIR_META_TYPE),1,metafptr);
+
+    this_meta.next_subdir_page = ftell(metafptr);
+    fseek(metafptr,0,SEEK_SET);
+
+    ret_val = fwrite(&this_meta,sizeof(DIR_META_TYPE),1,metafptr);
+
+    temppage.num_entries = 2;
+    temppage.dir_entries[0].d_ino = 1;
+    temppage.dir_entries[1].d_ino = 0;
+    strcpy(temppage.dir_entries[0].d_name,".");
+    strcpy(temppage.dir_entries[1].d_name,"..");
+
+    ret_val = fwrite(&temppage,sizeof(DIR_ENTRY_PAGE),1,metafptr);
+    fclose(metafptr);
+   }
+  return;
+ }
+
+
+
+int main(int argc, char **argv)
+ {
+  init_hfuse();
+  return hook_fuse(argc,argv);
+ }
