5fde5bacffe58574b3a561465e1e7daffce955ee
jiahongwu <jiahong.wu@delta.com.tw>
2014-05-12 18:32:52 +0800

    S3 backend

---
+++ HCFS_S3/hfuse_system.c
@@ -0,0 +1,234 @@
+#include "fuseop.h"
+#include "super_inode.h"
+#include "dir_lookup.h"
+#include "hcfscurl.h"
+#include "hcfs_tocloud.h"
+#include "hcfs_clouddelete.h"
+#include "params.h"
+#include <fuse.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/types.h>
+#include <attr/xattr.h>
+#include <openssl/hmac.h>
+#include <openssl/engine.h>
+
+
+int init_hcfs_system_data()
+ {
+  int shm_key;
+
+  shm_key = shmget(2345,sizeof(SYSTEM_DATA_HEAD), IPC_CREAT| 0666);
+  hcfs_system = shmat(shm_key, NULL, 0);
+
+  memset(hcfs_system,0,sizeof(SYSTEM_DATA_HEAD));
+  sem_init(&(hcfs_system->access_sem),1,1);
+  sem_init(&(hcfs_system->num_cache_sleep_sem),1,0);
+  sem_init(&(hcfs_system->check_cache_sem),1,0);
+  sem_init(&(hcfs_system->check_next_sem),1,0);
+  
+  hcfs_system->system_val_fptr = fopen(HCFSSYSTEM,"r+");
+  if (hcfs_system->system_val_fptr == NULL)
+   {
+    hcfs_system->system_val_fptr = fopen(HCFSSYSTEM,"w+");
+    fwrite(&(hcfs_system->systemdata),sizeof(SYSTEM_DATA_TYPE),1,hcfs_system->system_val_fptr);
+    fclose(hcfs_system->system_val_fptr);
+    hcfs_system->system_val_fptr = fopen(HCFSSYSTEM,"r+");
+   }
+  setbuf(hcfs_system->system_val_fptr,NULL);
+  fread(&(hcfs_system->systemdata),sizeof(SYSTEM_DATA_TYPE),1,hcfs_system->system_val_fptr);
+
+  return 0;
+ }
+
+int sync_hcfs_system_data(char need_lock)
+ {
+  if (need_lock == TRUE)
+   sem_wait(&(hcfs_system->access_sem));
+  fseek(hcfs_system->system_val_fptr,0,SEEK_SET);
+  fwrite(&(hcfs_system->systemdata),sizeof(SYSTEM_DATA_TYPE),1,hcfs_system->system_val_fptr);
+  if (need_lock == TRUE)
+   sem_post(&(hcfs_system->access_sem));
+
+  return 0;
+ }
+
+void init_hfuse()
+ {
+  int ret_val;
+  char rootmetapath[400];
+  ino_t root_inode;
+  struct stat this_stat;
+  DIR_META_TYPE this_meta;
+  DIR_ENTRY_PAGE temppage;
+  mode_t self_mode;
+  FILE *metafptr;
+
+  ret_val = super_inode_init();
+  init_pathname_cache();
+  ret_val = init_system_fh_table();
+  init_hcfs_system_data();
+
+  /* Check if need to initialize the root meta file */
+  fetch_meta_path(rootmetapath,1);
+  if (access(rootmetapath,F_OK)!=0)
+   {
+    memset(&this_stat,0,sizeof(struct stat));
+    memset(&this_meta,0,sizeof(DIR_META_TYPE));
+    memset(&temppage,0,sizeof(DIR_ENTRY_PAGE));
+
+    self_mode = S_IFDIR | 0777;
+    this_stat.st_mode = self_mode;
+    this_stat.st_nlink = 2;   /*One pointed by the parent, another by self*/
+    this_stat.st_uid = getuid();
+    this_stat.st_gid = getgid();
+    this_stat.st_atime = time(NULL);
+    this_stat.st_mtime = this_stat.st_atime;
+    this_stat.st_ctime = this_stat.st_ctime;
+
+    root_inode = super_inode_new_inode(&this_stat);
+    /*TODO: put error handling here if root_inode is not 1 (cannot initialize system)*/
+
+    this_stat.st_ino = 1;
+    memcpy(&(this_meta.thisstat),&this_stat,sizeof(struct stat));
+
+    metafptr = fopen(rootmetapath,"w");
+
+    ret_val = fwrite(&this_meta,sizeof(DIR_META_TYPE),1,metafptr);
+
+    this_meta.next_subdir_page = ftell(metafptr);
+    fseek(metafptr,0,SEEK_SET);
+
+    ret_val = fwrite(&this_meta,sizeof(DIR_META_TYPE),1,metafptr);
+
+    temppage.num_entries = 2;
+    temppage.dir_entries[0].d_ino = 1;
+    temppage.dir_entries[1].d_ino = 0;
+    strcpy(temppage.dir_entries[0].d_name,".");
+    strcpy(temppage.dir_entries[1].d_name,"..");
+
+    ret_val = fwrite(&temppage,sizeof(DIR_ENTRY_PAGE),1,metafptr);
+    fclose(metafptr);
+    super_inode_mark_dirty(1);
+   }
+  return;
+ }
+
+int main(int argc, char **argv)
+ {
+  CURL_HANDLE curl_handle;
+  int ret_val;
+  pid_t this_pid, this_pid1;
+  int download_handle_count;
+  struct rlimit nofile_limit;
+  pthread_t delete_loop_thread;
+  FILE *fptr;
+  char pathname[400];
+  char tempval[10];
+
+  sprintf(pathname,"%s/testfile",BLOCKPATH);
+
+  fptr=fopen(pathname,"w");
+  fprintf(fptr,"test\n");
+  fclose(fptr);
+  
+  ENGINE_load_builtin_engines();
+  ENGINE_register_all_complete();
+
+  ret_val = setxattr(pathname,"user.dirty","T",1,0);
+  if (ret_val < 0)
+   {
+    printf("Needs support for extended attributes, error no: %d\n",errno);
+    exit(-1);
+   }
+
+  tempval[0]=0;
+  getxattr(pathname,"user.dirty",(void *)tempval,1);
+  printf("test value is: %s, %d\n",tempval,strncmp(tempval,"T",1));
+  unlink(pathname);
+
+  nofile_limit.rlim_cur = 150000;
+  nofile_limit.rlim_max = 150001;
+  
+  ret_val = setrlimit(RLIMIT_NOFILE, &nofile_limit);
+  sprintf(curl_handle.id,"main");
+//  ret_val = hcfs_init_swift_backend(&curl_handle);
+  ret_val = hcfs_init_S3_backend(&curl_handle);
+  if ((ret_val < 200) || (ret_val > 299))
+   {
+    printf("error in connecting to backend\n");
+    exit(0);
+   }
+
+//  printf("%s\n %s\n %d\n",swift_auth_string,swift_url_string,ret_val);
+//  ret_val = hcfs_swift_list_container(&curl_handle);
+  ret_val = hcfs_S3_list_container(&curl_handle);
+  if ((ret_val < 200) || (ret_val > 299))
+   {
+    printf("error in connecting to backend\n");
+    exit(0);
+   }
+  printf("ret code %d\n",ret_val);
+//  hcfs_destroy_swift_backend(curl_handle.curl);
+  hcfs_destroy_S3_backend(curl_handle.curl);
+
+  return;
+
+  init_hfuse();
+  this_pid = fork();
+  if (this_pid == 0)
+   {
+    this_pid1 = fork();
+    if (this_pid1 == 0)
+     {
+      logfptr=fopen("swift_upload_log","a+");
+      fprintf(logfptr,"\nStart logging swift upload\n");
+      printf("Redirecting to swift log\n");
+      dup2(fileno(logfptr),fileno(stdout));
+      dup2(fileno(logfptr),fileno(stderr));
+      pthread_create(&delete_loop_thread,NULL,(void *)&delete_loop,NULL);
+      upload_loop();
+      fclose(logfptr);
+     }
+    else
+     {
+      logfptr=fopen("cache_maintain_log","a+");
+      fprintf(logfptr,"\nStart logging cache cleanup\n");
+      printf("Redirecting to cache log\n");
+      dup2(fileno(logfptr),fileno(stdout));
+      dup2(fileno(logfptr),fileno(stderr));
+
+      run_cache_loop();
+      fclose(logfptr);
+     }
+   }
+  else
+   {
+    logfptr=fopen("fuse_log","a+");
+    fprintf(logfptr,"\nStart logging fuse\n");
+    printf("Redirecting to fuse log\n");
+    dup2(fileno(logfptr),fileno(stdout));
+    dup2(fileno(logfptr),fileno(stderr));
+    sem_init(&download_curl_sem,0,MAX_DOWNLOAD_CURL_HANDLE);
+    sem_init(&download_curl_control_sem,0,1);
+
+    for(download_handle_count=0;download_handle_count<MAX_DOWNLOAD_CURL_HANDLE;download_handle_count++)
+     {
+      curl_handle_mask[download_handle_count]=FALSE;
+      ret_val = hcfs_init_swift_backend(&(download_curl_handles[download_handle_count]));
+      while ((ret_val < 200) || (ret_val > 299))
+       {
+        printf("error in connecting to swift\n");
+        if (download_curl_handles[download_handle_count].curl !=NULL)
+         hcfs_destroy_swift_backend(download_curl_handles[download_handle_count].curl);
+        ret_val = hcfs_init_swift_backend(&(download_curl_handles[download_handle_count]));
+
+       }
+     }
+    hook_fuse(argc,argv);
+    return;
+   }
+  return;
+ }
---
+++ HCFS_S3/metaops.c
@@ -0,0 +1,575 @@
+#include <sys/file.h>
+#include "fuseop.h"
+#include "params.h"
+/*TODO: Will need to check if need to explicitly change st_atime, st_mtime*/
+
+int dir_add_entry(ino_t parent_inode, ino_t child_inode, char *childname, mode_t child_mode)
+ {
+  FILE *parent_meta;
+  char parent_meta_name[400];
+  DIR_META_TYPE parent_meta_head;
+  DIR_ENTRY_PAGE temppage;
+  int ret_items;
+  off_t nextfilepos,oldfilepos;
+
+  fetch_meta_path(parent_meta_name,parent_inode);
+
+  parent_meta =fopen(parent_meta_name,"r+");
+  if (parent_meta==NULL)
+   return -1;
+  setbuf(parent_meta,NULL);
+  flock(fileno(parent_meta),LOCK_EX);
+  ret_items = fread(&parent_meta_head,sizeof(DIR_META_TYPE),1,parent_meta);
+  if (ret_items <1)
+   {
+    flock(fileno(parent_meta),LOCK_UN);
+    fclose(parent_meta);
+    return -1;
+   }
+
+  if (child_mode & S_IFREG)
+   {
+    nextfilepos=parent_meta_head.next_file_page;
+   }
+  else
+   {
+    if (child_mode & S_IFDIR)
+     nextfilepos=parent_meta_head.next_subdir_page;
+   }
+  memset(&temppage,0,sizeof(DIR_ENTRY_PAGE));
+  while(nextfilepos!=0)
+   {
+    fseek(parent_meta,nextfilepos,SEEK_SET);
+    if (ftell(parent_meta)!=nextfilepos)
+     {
+      flock(fileno(parent_meta),LOCK_UN);
+      fclose(parent_meta);
+      return -1;
+     }
+    ret_items = fread(&temppage,sizeof(DIR_ENTRY_PAGE),1,parent_meta);
+    if (ret_items <1)
+     {
+      flock(fileno(parent_meta),LOCK_UN);
+      fclose(parent_meta);
+      return -1;
+     }
+    if (temppage.num_entries < MAX_DIR_ENTRIES_PER_PAGE)
+     {   /*This page has empty dir entry. Fill it.*/
+      temppage.dir_entries[temppage.num_entries].d_ino = child_inode;
+      strcpy(temppage.dir_entries[temppage.num_entries].d_name,childname);
+      temppage.num_entries++;
+      fseek(parent_meta,nextfilepos,SEEK_SET);
+      if (ftell(parent_meta)!=nextfilepos)
+       {
+        flock(fileno(parent_meta),LOCK_UN);
+        fclose(parent_meta);
+        return -1;
+       }
+      ret_items = fwrite(&temppage,sizeof(DIR_ENTRY_PAGE),1,parent_meta);
+      if (ret_items <1)
+       {
+        flock(fileno(parent_meta),LOCK_UN);
+        fclose(parent_meta);
+        return -1;
+       }
+      /*If the new entry is a subdir, increase the hard link of the parent*/
+
+      if (child_mode & S_IFDIR)
+       {
+        parent_meta_head.thisstat.st_nlink++;
+       }
+
+      parent_meta_head.total_children++;
+      printf("TOTAL CHILDREN is now %ld\n",parent_meta_head.total_children);
+
+      fseek(parent_meta,0,SEEK_SET);
+      ret_items = fwrite(&parent_meta_head,sizeof(DIR_META_TYPE),1,parent_meta);
+      if (ret_items <1)
+       {
+        flock(fileno(parent_meta),LOCK_UN);
+        fclose(parent_meta);
+        return -1;
+       }
+
+ 
+      flock(fileno(parent_meta),LOCK_UN);
+      fclose(parent_meta);
+
+      super_inode_mark_dirty(parent_inode);
+      return 0;
+     }
+    if (temppage.next_page == 0)
+     break;
+    nextfilepos = temppage.next_page;
+   }
+
+  /*Will need to allocate a new page*/
+  oldfilepos = nextfilepos;
+
+  fseek(parent_meta,0,SEEK_END);
+  nextfilepos=ftell(parent_meta);
+
+  if (oldfilepos!=0)
+   {
+    temppage.next_page = nextfilepos;
+    fseek(parent_meta,oldfilepos,SEEK_SET);
+    ret_items = fwrite(&temppage,sizeof(DIR_ENTRY_PAGE),1,parent_meta);
+    if (ret_items <1)
+     {
+      flock(fileno(parent_meta),LOCK_UN);
+      fclose(parent_meta);
+      return -1;
+     }
+    if (child_mode & S_IFDIR)
+     {
+      parent_meta_head.thisstat.st_nlink++;
+     }
+   }
+  else
+   {
+    if (child_mode & S_IFREG)
+     {
+      parent_meta_head.next_file_page=nextfilepos;
+     }
+    else
+     {
+      if (child_mode & S_IFDIR)
+       {
+        parent_meta_head.next_subdir_page=nextfilepos;
+        parent_meta_head.thisstat.st_nlink++;
+       }
+     }
+   }
+  fseek(parent_meta,nextfilepos,SEEK_SET);
+  memset(&temppage,0,sizeof(DIR_ENTRY_PAGE));
+  temppage.num_entries=1;
+  temppage.dir_entries[0].d_ino = child_inode;
+  strcpy(temppage.dir_entries[0].d_name,childname);
+  ret_items = fwrite(&temppage,sizeof(DIR_ENTRY_PAGE),1,parent_meta);
+  if (ret_items <1)
+   {
+    flock(fileno(parent_meta),LOCK_UN);
+    fclose(parent_meta);
+    return -1;
+   }
+
+  parent_meta_head.total_children++;
+  printf("TOTAL CHILDREN is now %ld\n",parent_meta_head.total_children);
+
+  fseek(parent_meta,0,SEEK_SET);
+  ret_items = fwrite(&parent_meta_head,sizeof(DIR_META_TYPE),1,parent_meta);
+  if (ret_items <1)
+   {
+    flock(fileno(parent_meta),LOCK_UN);
+    fclose(parent_meta);
+    return -1;
+   }
+
+
+  flock(fileno(parent_meta),LOCK_UN);
+  fclose(parent_meta);
+
+  super_inode_mark_dirty(parent_inode);
+
+  return 0;
+ }
+
+
+int dir_remove_entry(ino_t parent_inode, ino_t child_inode, char *childname, mode_t child_mode)
+ {
+  FILE *parent_meta;
+  char parent_meta_name[400];
+  DIR_META_TYPE parent_meta_head;
+  DIR_ENTRY_PAGE temppage;
+  int ret_items;
+  off_t nextfilepos,oldfilepos;
+  int count;
+
+  fetch_meta_path(parent_meta_name,parent_inode);
+
+  parent_meta =fopen(parent_meta_name,"r+");
+  if (parent_meta==NULL)
+   return -1;
+  setbuf(parent_meta,NULL);
+  flock(fileno(parent_meta),LOCK_EX);
+  ret_items = fread(&parent_meta_head,sizeof(DIR_META_TYPE),1,parent_meta);
+  if (ret_items <1)
+   {
+    flock(fileno(parent_meta),LOCK_UN);
+    fclose(parent_meta);
+    return -1;
+   }
+
+  if (child_mode & S_IFREG)
+   {
+    nextfilepos=parent_meta_head.next_file_page;
+   }
+  else
+   {
+    if (child_mode & S_IFDIR)
+     nextfilepos=parent_meta_head.next_subdir_page;
+   }
+  memset(&temppage,0,sizeof(DIR_ENTRY_PAGE));
+  while(nextfilepos!=0)
+   {
+    fseek(parent_meta,nextfilepos,SEEK_SET);
+    if (ftell(parent_meta)!=nextfilepos)
+     {
+      flock(fileno(parent_meta),LOCK_UN);
+      fclose(parent_meta);
+      return -1;
+     }
+    ret_items = fread(&temppage,sizeof(DIR_ENTRY_PAGE),1,parent_meta);
+    if (ret_items <1)
+     {
+      flock(fileno(parent_meta),LOCK_UN);
+      fclose(parent_meta);
+      return -1;
+     }
+
+    for(count=0;count<temppage.num_entries;count++)
+     {
+      if ((strcmp(temppage.dir_entries[count].d_name,childname)==0) &&
+           (temppage.dir_entries[count].d_ino == child_inode))
+       {  /*Found the entry. Delete it*/
+        if (count!=(temppage.num_entries-1))
+         memcpy(&(temppage.dir_entries[count]), &(temppage.dir_entries[temppage.num_entries-1]), sizeof(DIR_ENTRY));
+        temppage.num_entries--;
+        memset(&(temppage.dir_entries[temppage.num_entries]),0,sizeof(DIR_ENTRY));
+
+        fseek(parent_meta,nextfilepos,SEEK_SET);
+        if (ftell(parent_meta)!=nextfilepos)
+         {
+          flock(fileno(parent_meta),LOCK_UN);
+          fclose(parent_meta);
+          return -1;
+         }
+        ret_items = fwrite(&temppage,sizeof(DIR_ENTRY_PAGE),1,parent_meta);
+        if (ret_items <1)
+         {
+          flock(fileno(parent_meta),LOCK_UN);
+          fclose(parent_meta);
+          return -1;
+         }
+      /*If the new entry is a subdir, decrease the hard link of the parent*/
+
+        if (child_mode & S_IFDIR)
+         {
+          parent_meta_head.thisstat.st_nlink--;
+          fseek(parent_meta,0,SEEK_SET);
+          ret_items = fwrite(&parent_meta_head,sizeof(DIR_META_TYPE),1,parent_meta);
+          if (ret_items <1)
+           {
+            flock(fileno(parent_meta),LOCK_UN);
+            fclose(parent_meta);
+            return -1;
+           }
+         }
+ 
+        parent_meta_head.total_children--;
+        printf("TOTAL CHILDREN is now %ld\n",parent_meta_head.total_children);
+        fseek(parent_meta,0,SEEK_SET);
+        ret_items = fwrite(&parent_meta_head,sizeof(DIR_META_TYPE),1,parent_meta);
+        if (ret_items <1)
+         {
+          flock(fileno(parent_meta),LOCK_UN);
+          fclose(parent_meta);
+          return -1;
+         }
+
+        flock(fileno(parent_meta),LOCK_UN);
+        fclose(parent_meta);
+
+        super_inode_mark_dirty(parent_inode);
+
+        return 0;
+       }
+     }
+    if (temppage.next_page == 0)
+     break;
+    nextfilepos = temppage.next_page;
+   }
+
+  flock(fileno(parent_meta),LOCK_UN);
+  fclose(parent_meta);
+  return -1;
+ }
+
+int dir_replace_name(ino_t parent_inode, ino_t child_inode, char *oldname, char *newname, mode_t child_mode)
+ {
+  FILE *parent_meta;
+  char parent_meta_name[400];
+  DIR_META_TYPE parent_meta_head;
+  DIR_ENTRY_PAGE temppage;
+  int ret_items;
+  off_t nextfilepos,oldfilepos;
+  int count;
+
+  fetch_meta_path(parent_meta_name,parent_inode);
+
+  parent_meta =fopen(parent_meta_name,"r+");
+  if (parent_meta==NULL)
+   return -1;
+  setbuf(parent_meta,NULL);
+  flock(fileno(parent_meta),LOCK_EX);
+  ret_items = fread(&parent_meta_head,sizeof(DIR_META_TYPE),1,parent_meta);
+  if (ret_items <1)
+   {
+    flock(fileno(parent_meta),LOCK_UN);
+    fclose(parent_meta);
+    return -1;
+   }
+
+  if (child_mode & S_IFREG)
+   {
+    nextfilepos=parent_meta_head.next_file_page;
+   }
+  else
+   {
+    if (child_mode & S_IFDIR)
+     nextfilepos=parent_meta_head.next_subdir_page;
+   }
+  memset(&temppage,0,sizeof(DIR_ENTRY_PAGE));
+  while(nextfilepos!=0)
+   {
+    fseek(parent_meta,nextfilepos,SEEK_SET);
+    if (ftell(parent_meta)!=nextfilepos)
+     {
+      flock(fileno(parent_meta),LOCK_UN);
+      fclose(parent_meta);
+      return -1;
+     }
+    ret_items = fread(&temppage,sizeof(DIR_ENTRY_PAGE),1,parent_meta);
+    if (ret_items <1)
+     {
+      flock(fileno(parent_meta),LOCK_UN);
+      fclose(parent_meta);
+      return -1;
+     }
+
+    for(count=0;count<temppage.num_entries;count++)
+     {
+      if ((strcmp(temppage.dir_entries[count].d_name,oldname)==0) &&
+           (temppage.dir_entries[count].d_ino == child_inode))
+       {  /*Found the entry. Replace it*/
+        strcpy(temppage.dir_entries[count].d_name,newname);
+
+        fseek(parent_meta,nextfilepos,SEEK_SET);
+        if (ftell(parent_meta)!=nextfilepos)
+         {
+          flock(fileno(parent_meta),LOCK_UN);
+          fclose(parent_meta);
+          return -1;
+         }
+        ret_items = fwrite(&temppage,sizeof(DIR_ENTRY_PAGE),1,parent_meta);
+        if (ret_items <1)
+         {
+          flock(fileno(parent_meta),LOCK_UN);
+          fclose(parent_meta);
+          return -1;
+         }
+ 
+        flock(fileno(parent_meta),LOCK_UN);
+        fclose(parent_meta);
+        super_inode_mark_dirty(parent_inode);
+
+        return 0;
+       }
+     }
+    if (temppage.next_page == 0)
+     break;
+    nextfilepos = temppage.next_page;
+   }
+
+  flock(fileno(parent_meta),LOCK_UN);
+  fclose(parent_meta);
+  return -1;
+ }
+
+int change_parent_inode(ino_t self_inode, ino_t parent_inode1, ino_t parent_inode2)
+ {
+  FILE *self_meta;
+  char self_meta_name[400];
+  DIR_META_TYPE self_meta_head;
+  DIR_ENTRY_PAGE temppage;
+  int ret_items;
+  int count;
+
+  fetch_meta_path(self_meta_name,self_inode);
+
+  self_meta =fopen(self_meta_name,"r+");
+  if (self_meta==NULL)
+   return -1;
+  setbuf(self_meta,NULL);
+  flock(fileno(self_meta),LOCK_EX);
+  ret_items = fread(&self_meta_head,sizeof(DIR_META_TYPE),1,self_meta);
+  if (ret_items <1)
+   {
+    flock(fileno(self_meta),LOCK_UN);
+    fclose(self_meta);
+    return -1;
+   }
+
+  memset(&temppage,0,sizeof(DIR_ENTRY_PAGE));
+  fseek(self_meta,self_meta_head.next_subdir_page,SEEK_SET);
+  if (ftell(self_meta)!=self_meta_head.next_subdir_page)
+   {
+    flock(fileno(self_meta),LOCK_UN);
+    fclose(self_meta);
+    return -1;
+   }
+  ret_items = fread(&temppage,sizeof(DIR_ENTRY_PAGE),1,self_meta);
+  if (ret_items <1)
+   {
+    flock(fileno(self_meta),LOCK_UN);
+    fclose(self_meta);
+    return -1;
+   }
+
+  for(count=0;count<temppage.num_entries;count++)
+   {
+    if (strcmp(temppage.dir_entries[count].d_name,"..")==0)
+     {
+      if (temppage.dir_entries[count].d_ino!=parent_inode1)
+       {
+        flock(fileno(self_meta),LOCK_UN);
+        fclose(self_meta);
+        return -1;
+       }
+      temppage.dir_entries[count].d_ino = parent_inode2;
+      fseek(self_meta,self_meta_head.next_subdir_page,SEEK_SET);
+      if (ftell(self_meta)!=self_meta_head.next_subdir_page)
+       {
+        flock(fileno(self_meta),LOCK_UN);
+        fclose(self_meta);
+        return -1;
+       }
+      ret_items = fwrite(&temppage,sizeof(DIR_ENTRY_PAGE),1,self_meta);
+      if (ret_items <1)
+       {
+        flock(fileno(self_meta),LOCK_UN);
+        fclose(self_meta);
+        return -1;
+       }
+ 
+      flock(fileno(self_meta),LOCK_UN);
+      fclose(self_meta);
+      super_inode_mark_dirty(self_inode);
+
+      return 0;
+     }
+   }
+  flock(fileno(self_meta),LOCK_UN);
+  fclose(self_meta);
+  return -1;
+ }
+
+int decrease_nlink_inode_file(ino_t this_inode)
+ {
+  char thismetapath[400];
+  char todelete_metapath[400];
+  char thisblockpath[400];
+  char filebuf[5000];
+  FILE_META_TYPE this_meta;
+  FILE *metafptr;
+  FILE *todeletefptr;
+  int ret_val;
+  long long count;
+  long long total_blocks;
+  off_t cache_block_size;
+  size_t read_size;
+
+  fetch_meta_path(thismetapath,this_inode);
+
+  metafptr = fopen(thismetapath,"r+");
+  if (metafptr == NULL)
+   return -EACCES;
+  setbuf(metafptr,NULL);
+  flock(fileno(metafptr),LOCK_EX);
+
+  ret_val = fread(&this_meta,sizeof(FILE_META_TYPE),1,metafptr);
+  if (ret_val < 1)
+   {
+    flock(fileno(metafptr),LOCK_UN);
+    fclose(metafptr);
+    return -EACCES;
+   }
+
+  if (this_meta.thisstat.st_nlink<=1)
+   {
+    /*Need to delete the inode*/
+    super_inode_to_delete(this_inode);
+    fetch_todelete_path(todelete_metapath,this_inode);
+    /*Try a rename first*/
+    ret_val = rename(thismetapath,todelete_metapath);
+    if (ret_val < 0)
+     {
+      /*If not successful, copy the meta*/
+      unlink(todelete_metapath);
+      todeletefptr = fopen(todelete_metapath,"w");
+      fseek(metafptr,0,SEEK_SET);
+      while(!feof(metafptr))
+       {
+        read_size = fread(filebuf,1,4096,metafptr);
+        if (read_size > 0)
+         {
+          fwrite(filebuf,1,read_size,todeletefptr);
+         }
+        else
+         break;
+       }
+      fclose(todeletefptr);
+
+      unlink(thismetapath);
+     }
+
+    /*Need to delete blocks as well*/
+    /*TODO: Perhaps can move the actual block deletion to the deletion loop as well*/
+    if (this_meta.thisstat.st_size == 0)
+     total_blocks = 0;
+    else
+     total_blocks = ((this_meta.thisstat.st_size-1) / MAX_BLOCK_SIZE) + 1;
+    for(count = 0;count<total_blocks;count++)
+     {
+      fetch_block_path(thisblockpath,this_inode,count);
+      if (!access(thisblockpath,F_OK))
+       {
+        cache_block_size = check_file_size(thisblockpath);
+        unlink(thisblockpath);
+        sem_wait(&(hcfs_system->access_sem));
+        hcfs_system->systemdata.cache_size -= (long long) cache_block_size;
+        hcfs_system->systemdata.cache_blocks -=1;
+        sem_post(&(hcfs_system->access_sem));           
+       }
+     }
+    sem_wait(&(hcfs_system->access_sem));
+    hcfs_system->systemdata.system_size -= this_meta.thisstat.st_size;
+    sync_hcfs_system_data(FALSE);
+    sem_post(&(hcfs_system->access_sem));           
+
+    flock(fileno(metafptr),LOCK_UN);
+    fclose(metafptr);
+
+   }
+  else
+   {
+    this_meta.thisstat.st_nlink--; 
+    fseek(metafptr,0,SEEK_SET);
+    ret_val = fwrite(&this_meta,sizeof(FILE_META_TYPE),1,metafptr);
+    if (ret_val < 1)
+     {
+      flock(fileno(metafptr),LOCK_UN);
+      fclose(metafptr);
+      return -EACCES;
+     }
+    super_inode_update_stat(this_inode, &(this_meta.thisstat));
+    flock(fileno(metafptr),LOCK_UN);
+
+    super_inode_mark_dirty(this_inode);
+
+    fclose(metafptr);
+   }
+
+  return 0;
+ }    
+
