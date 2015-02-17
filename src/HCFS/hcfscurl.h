2c7ab57fd7c724f4ea90ada76a5aad6848f292e5
jiahongwu <jiahong.wu@hopebaytech.com>
2015-02-17 15:49:02 +0800

    code style fix

--- src/HCFS/hcfscurl.h
+++ src/HCFS/hcfscurl.h
@@ -1,3 +1,15 @@
+/*************************************************************************
+*
+* Copyright © 2014-2015 Hope Bay Technologies, Inc. All rights reserved.
+*
+* File Name: hcfscurl.h
+* Abstract: The c header file for CURL operations.
+*
+* Revision History
+* 2015/2/17 Jiahong added header for this file, and revising coding style.
+*
+**************************************************************************/
+
 #ifndef GW20_HCFS_HCFSCURL_H_
 #define GW20_HCFS_HCFSCURL_H_
 
@@ -17,14 +29,15 @@
 #define S3_BUCKET "testgateway"
 #define S3_BUCKET_URL "https://testgateway.s3.hicloud.net.tw"
 
- 
 #define MAX_DOWNLOAD_CURL_HANDLE 16
 
 typedef struct {
-    CURL *curl;
-    char id[256];  /*A short name representing the unique identity of the handle*/
-    int curl_backend;
-  } CURL_HANDLE;
+	CURL *curl;
+
+	/*A short name representing the unique identity of the handle*/
+	char id[256];
+	int curl_backend;
+} CURL_HANDLE;
 
 char swift_auth_string[1024];
 char swift_url_string[1024];
@@ -36,7 +49,8 @@ sem_t download_curl_sem;
 
 /* Swift collections */
 
-int hcfs_get_auth_swift(char *swift_user,char *swift_pass, char *swift_url, CURL_HANDLE *curl_handle);
+int hcfs_get_auth_swift(char *swift_user, char *swift_pass, char *swift_url,
+						CURL_HANDLE *curl_handle);
 int hcfs_init_swift_backend(CURL_HANDLE *curl_handle);
 void hcfs_destroy_swift_backend(CURL *curl);
 int hcfs_swift_list_container(CURL_HANDLE *curl_handle);
@@ -44,7 +58,6 @@ int hcfs_swift_put_object(FILE *fptr, char *objname, CURL_HANDLE *curl_handle);
 int hcfs_swift_get_object(FILE *fptr, char *objname, CURL_HANDLE *curl_handle);
 int hcfs_swift_reauth(CURL_HANDLE *curl_handle);
 int hcfs_swift_delete_object(char *objname, CURL_HANDLE *curl_handle);
-
 
 /* S3 collections */
 int hcfs_init_S3_backend(CURL_HANDLE *curl_handle);
