0fe5478974e5f127cd222679e5746c97dd3bc1cc
root <root@ubuntu.(none)>
2015-02-26 17:06:33 +0800

    Fix type conflict

--- tests/unit_test/c/unittest_modules/meta_processing/unittests/mock_function.c
+++ tests/unit_test/c/unittest_modules/meta_processing/unittests/mock_function.c
@@ -15,10 +15,9 @@ int meta_cache_lookup_dir_data(ino_t this_inode, struct stat *inode_stat,
 	return 0;
 }
 
-
-int meta_cache_update_dir_data(ino_t this_inode, struct stat *inode_stat,
-	DIR_META_TYPE *dir_meta_ptr, DIR_ENTRY_PAGE *dir_page,
-	META_CACHE_ENTRY_STRUCT *body_ptr)
+int meta_cache_update_dir_data(ino_t this_inode, const struct stat *inode_stat,
+    const DIR_META_TYPE *dir_meta_ptr, const DIR_ENTRY_PAGE *dir_page,
+    META_CACHE_ENTRY_STRUCT *body_ptr)
 {
 	return 0;
 }
@@ -84,10 +83,9 @@ int meta_cache_lookup_file_data(ino_t this_inode, struct stat *inode_stat,
 	return 0;
 }
 
-
-int meta_cache_update_file_data(ino_t this_inode, struct stat *inode_stat,
-	FILE_META_TYPE *file_meta_ptr, BLOCK_ENTRY_PAGE *block_page,
-	long long page_pos, META_CACHE_ENTRY_STRUCT *body_ptr)
+int meta_cache_update_file_data(ino_t this_inode, const struct stat *inode_stat,
+    const FILE_META_TYPE *file_meta_ptr, const BLOCK_ENTRY_PAGE *block_page,
+    const long long page_pos, META_CACHE_ENTRY_STRUCT *body_ptr)
 {
 	return 0;
 }
