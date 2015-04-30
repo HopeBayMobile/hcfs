71afda76d03f3c9388dcb91677e5b410533e4c1e
kewei <kewei.chen@happygorgi.com>
2015-04-30 18:54:52 +0800

    part of mock function

--- tests/unit_test/c/unittest_modules/cloud_get_put/unittests/hcfs_tocloud_unittest.cc
+++ tests/unit_test/c/unittest_modules/cloud_get_put/unittests/hcfs_tocloud_unittest.cc
@@ -19,12 +19,12 @@ TEST(init_upload_controlTest, DoNothing_JustRun)
 	
 	/* Check */
 	EXPECT_EQ(0, upload_ctl.total_active_upload_threads);
-	EXPECT_EQ(0, memcmp(zero_mem, &(upload_ctl.threads_in_use)));
-	EXPECT_EQ(0, memcmp(zero_mem, &(upload_ctl.threads_created)));
+	EXPECT_EQ(0, memcmp(zero_mem, &(upload_ctl.threads_in_use), sizeof(zero_mem)));
+	EXPECT_EQ(0, memcmp(zero_mem, &(upload_ctl.threads_created), sizeof(zero_mem)));
 	
 	/* Free resource */
-	EXPECT_EQ(0, pthread_cancel(upload_ctl.upload_handle_thread));
-	EXPECT_EQ(0, pthread_join(upload_ctl.upload_handle_thread, &res));
+	EXPECT_EQ(0, pthread_cancel(upload_ctl.upload_handler_thread));
+	EXPECT_EQ(0, pthread_join(upload_ctl.upload_handler_thread, &res));
 	EXPECT_EQ(PTHREAD_CANCELED, res);
 
 
--- tests/unit_test/c/unittest_modules/cloud_get_put/unittests/tocloud_mock_function.c
+++ tests/unit_test/c/unittest_modules/cloud_get_put/unittests/tocloud_mock_function.c
@@ -1,5 +1,10 @@
 #include <sys/stat.h>
+#include "hcfs_tocloud.h"
 #include "hcfs_clouddelete.h"
+#include "params.h"
+#include "mock_params.h"
+
+SYSTEM_CONF_STRUCT system_config;
 
 int fetch_meta_path(char *pathname, ino_t this_inode)
 {
@@ -14,4 +19,14 @@ int fetch_block_path(char *pathname, ino_t this_inode, long long block_num)
 void con_object_dsync(DELETE_THREAD_TYPE *delete_thread_ptr)
 {
 	return;
+}
+
+int hcfs_init_backend(CURL_HANDLE *curl_handle)
+{
+	return HTTP_OK;
+}
+
+int super_block_update_transit(ino_t this_inode, char is_start_transit)
+{
+	return 0;
 }
