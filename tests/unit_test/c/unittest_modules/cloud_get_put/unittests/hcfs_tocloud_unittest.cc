2df80d406a24f5ada8472e5f4336c1efe4b026b1
kewei <kewei.chen@happygorgi.com>
2015-06-08 16:18:14 +0800

    Add assertion

--- tests/unit_test/c/unittest_modules/cloud_get_put/unittests/hcfs_tocloud_unittest.cc
+++ tests/unit_test/c/unittest_modules/cloud_get_put/unittests/hcfs_tocloud_unittest.cc
@@ -603,9 +603,14 @@ TEST(upload_loopTest, UploadLoopWorkSuccess_OnlyTestDirCase)
 	
 	/* Allocate a share space to store actual value */
 	shm_key = shmget(5566, sizeof(LoopToVerifiedData), IPC_CREAT | 0666);
+	ASSERT_NE(-1, shm_key);
 	shm_verified_data = (LoopToVerifiedData *) shmat(shm_key, NULL, 0);
+	ASSERT_NE((void *)-1, shm_verified_data);
+	
 	shm_key2 = shmget(1144, sizeof(int)*shm_test_data->num_inode, IPC_CREAT | 0666);
+	ASSERT_NE(-1, shm_key2);
 	shm_verified_data->record_handle_inode = (int *)shmat(shm_key2, NULL, 0);
+	ASSERT_NE((void *)-1, shm_verified_data->record_handle_inode);
 	shm_verified_data->record_inode_counter = 0;
 	sem_init(&(shm_verified_data->record_inode_sem), 0, 1);
 	
