4ba27ac7368339e63ccfa330bc9d0a71e79983b2
Li-Ting Tsai <lt.tsai@hopebaytech.com>
2015-08-13 17:44:50 +0800

    add enc unittest folder

---
+++ tests/unit_test/c/unittest_modules/enc/unittests/hcfs_enc_unittest.cc
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+#include <string.h>
+extern "C" {
+#include "b64encode.h"
+#include "enc.h"
+}
+
+
+TEST(base64, encode_then_decode)
+{
+	const char* input = "4ytg0jkk0234]yhj]-43jddfv1111";
+	int length_encode = expect_b64_encode_length(strlen(input));
+	char* code = (char*)calloc(length_encode, sizeof(char));
+	int out_len = 0;
+	b64encode_str((unsigned char*)input, (unsigned char*)code, &out_len, strlen(input));
+	unsigned char* decode = (unsigned char*)calloc(out_len+1, sizeof(unsigned char));
+	b64decode_str(code, decode, &out_len, out_len);
+	EXPECT_EQ(decode[0], '4');
+	EXPECT_EQ(decode[4], '0');
+	EXPECT_EQ(decode[8], '0');
+	EXPECT_EQ(decode[12], ']');
+	EXPECT_EQ(decode[16], ']');
+	free(code);
+	free(decode);
+
+}
