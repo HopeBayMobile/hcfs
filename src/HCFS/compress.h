365558c65ad52ee4ef3603ac84326cbb12c29ee8
Li-Ting Tsai <lt.tsai@hopebaytech.com>
2015-08-19 15:51:24 +0800

    remove unnecessary part now

--- src/HCFS/compress.h
+++ src/HCFS/compress.h
@@ -1,9 +1,4 @@
 #include <lz4.h>
-/*
-#include <inttypes.h>
-#include <lzma.h>
-#include <zlib.h>
-*/
 
 typedef
 int (*compress_func)(const char* source, char* dest, int inputSize);
@@ -15,16 +10,3 @@ int (*decompress_func)(const char* source, char* dest, int inputSize,
 extern compress_func compress_f;
 extern decompress_func decompress_f;
 
-/*
-static
-int lzma_compress_func (const char* source, char* dest, int inputSize);
-
-static
-int lzma_decompress_func (const char* source, char* dest, int inputSize, int maxOutputSize);
-
-static
-int zlib_compress_func (const char* source, char* dest, int inputSize);
-
-static
-int zlib_decompress_func (const char* source, char* dest, int inputSize, int maxOutputSize);
-*/
