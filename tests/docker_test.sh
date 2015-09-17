c4318882a6074f4bac7d0ce8c8e806418ada9e2f
Jethro Yu <jethro.yu@hopebaytech.com>
2015-09-18 06:33:33 +0800

    reindent docker_test.sh

--- tests/docker_test.sh
+++ tests/docker_test.sh
@@ -12,20 +12,27 @@ $local_repo/utils/setup_dev_env.sh -v -m tests
 sudo docker rm -f swift_test || true
 sudo rm -rf /tmp/swift_data
 sudo mkdir -p /tmp/swift_data
-SWIFT_ID=$(sudo docker run -d -v /tmp/swift_data:/srv --name=swift_test -t aerofs/swift)
+SWIFT_ID=$(sudo docker run -d -t \
+		-v /tmp/swift_data:/srv \
+		--name=swift_test \
+		aerofs/swift)
 SWIFT_IP=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' $SWIFT_ID)
 
 # Start test slave
 sudo docker rm -f hcfs_test 2>/dev/null || true
-SLAVE_ID=$(sudo docker run --privileged -v $local_repo:/home/jenkins/workspace/HCFS \
-		-d --name=hcfs_test --link swift_test docker:5000/docker_hcfs_test_slave)
+SLAVE_ID=$(sudo docker run -d -t \
+		--privileged \
+		-v $local_repo:/home/jenkins/workspace/HCFS \
+		--name=hcfs_test \
+		--link swift_test \
+		docker:5000/docker_hcfs_test_slave)
 SLAVE_IP=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' $SLAVE_ID)
 
 # Inject SSH key into test slave
 mkdir -p $local_repo/tmp
 key=$local_repo/tmp/id_rsa
 [ ! -f $key ] && ssh-keygen -f $key -N ""
-sudo docker exec -i hcfs_test /bin/bash -c 'cat >> /home/jenkins/.ssh/authorized_keys' < ${key}.pub
+sudo docker exec -i $SLAVE_ID /bin/bash -c 'cat >> /home/jenkins/.ssh/authorized_keys' < ${key}.pub
 
 # Wait test slave ready
 SSH="ssh -o UserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i $key jenkins@$SLAVE_IP"
