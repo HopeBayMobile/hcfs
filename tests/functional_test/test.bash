9aae14bb5859e421de9a9b285f9949856386dab0
Jethro Yu <jethro.yu@hopebaytech.com>
2015-10-01 19:38:59 +0800

    Reload script if user does not have effective fuse group

--- tests/functional_test/test.bash
+++ tests/functional_test/test.bash
@@ -5,23 +5,22 @@ here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "########## Setup Test Env"
 $WORKSPACE/utils/setup_dev_env.sh -m functional_test
-. $WORKSPACE/utils/env_config.sh
-. $WORKSPACE/utils/trace_error.bash
-set -e
 if [[ $(id -Gn) != *fuse* ]]; then
 	echo "########## Reload script with fuse group permission"
 	exec sg fuse "${BASH_SOURCE[0]}"
 fi
 id
 groups
+. $WORKSPACE/utils/env_config.sh
+. $WORKSPACE/utils/trace_error.bash
+set -e
 
-$here/Scripts/compile_hcfs_bin.bash
 # Setup for $hcfs and $HCFSvol
+$here/Scripts/compile_hcfs_bin.bash
 . $here/Scripts/path_config.sh
 
-
 $here/Scripts/start_swift.bash
-$here/Scripts/test_mount.bash
+#$here/Scripts/test_mount.bash
 
 cd "$here"
-#python pi_tester.py -d debug -c HCFS_0
+python pi_tester.py -d debug -c HCFS_0
