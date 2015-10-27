09a2f84582a3c99db06bbd3ee0e3923acd2cbe9d
jiahongwu <jiahong.wu@hopebaytech.com>
2015-10-27 19:31:55 +0800

    Working on looking up path from inode

--- src/HCFS/path_reconstruct.c
+++ src/HCFS/path_reconstruct.c
@@ -20,46 +20,201 @@
 /************************************************************************
 *
 * Function name: init_pathcache
-*        Inputs: None
-*       Summary: Init cache for inode to path lookup
-*  Return value: 0 if successful. Otherwise returns the negation of the
-*                appropriate error code.
+*        Inputs: ino_t root_inode
+*       Summary: Allocate a new cache for inode to path lookup
+*  Return value: Pointer to the new path cache structure if successful.
+*                NULL otherwise.
 *
 *************************************************************************/
-int init_pathcache()
+PATH_CACHE * init_pathcache(ino_t root_inode);
 {
 	int ret, errcode;
+	PATH_CACHE *tmpptr;
 
-	ret = sem_init(&pathcache_lock, 0, 1);
-	if (ret < 0) {
-		errcode = errno;
-		write_log(0, "Error: %d, %s\n", errcode, strerror(errcode));
-		errcode = -errcode;
+	tmpptr = malloc(sizeof(PATH_CACHE));
+	if (tmpptr == NULL) {
+		write_log(0, "Out of memory\n");
 		goto errcode_handle;
 	}
 
-	memset(pathcache, 0, sizeof(PATH_HEAD_ENTRY) * NUM_LOOKUP_ENTRY);
+	ret = sem_init(&(tmpptr->pathcache_lock), 0, 1);
+	if (ret < 0) {
+		errcode = errno;
+		write_log(0, "Error: %d, %s\n", errcode, strerror(errcode));
+		goto errcode_handle;
+	}
 
-	return 0;
+	memset((tmpptr->pathcache), 0,
+		sizeof(PATH_HEAD_ENTRY) * NUM_LOOKUP_ENTRY);
+	tmpptr->gfirst = NULL;
+	tmpptr->num_nodes = 0;
+	tmpptr->root_inode = root_inode;
+
+	return tmpptr;
+
 errcode_handle:
-	return errcode;
+	if (tmpptr != NULL)
+		free(tmpptr);
+	return NULL;
 }
 /************************************************************************
 *
 * Function name: destroy_pathcache
-*        Inputs: None
-*       Summary: Free up cache for inode to path lookup
-*  Return value: 0 if successful. Otherwise returns the negation of the
-*                appropriate error code.
+*        Inputs: PATH_CACHE *cacheptr
+*       Summary: Free up cache pointed by "cacheptr" for inode to path lookup
+*  Return value: 0 if successful. Otherwise returns negation of error code.
 *
 *************************************************************************/
-int destroy_pathcache()
+int destroy_pathcache(PATH_CACHE *cacheptr)
 {
+	PATH_LOOKUP *tmp, *next;
+	int count, ret, errcode;
+
+	if (cacheptr == NULL)
+		return -EINVAL;
+
+	ret = sem_wait(&(cacheptr->pathcache_lock));
+	if (ret < 0) {
+		errcode = errno;
+		write_log(0, "Unexpected error. Code %d, %s\n",
+			errcode, strerror(errcode));
+		errcode = -errcode;
+		goto errcode_handle;
+	}
+
+	for (count = 0; count < NUM_LOOKUP_ENTRY; count++) {
+		tmp = (cacheptr->pathcache[count]).first;
+
+		while (tmp != NULL) {
+			next = tmp->next;
+			free(tmp);
+			tmp = next;
+		}
+	}
+	sem_post(&(cacheptr->pathcache_lock));
+	free(cacheptr);
+	return 0;
+
+errcode_handle:
+	return errcode;
+}
+
+/* Internal function for dropping a certain number of least-referenced
+entries */
+void drop_cache_entry(PATH_CACHE *cacheptr)
+{
+	int num_dropped, hashval;
+	PATH_LOOKUP *tmpptr;
+	int ret, errcode;
+
+	num_dropped = 0;
+	tmpptr = cacheptr->gfirst;
+	while (num_dropped < NUM_NODES_DROP) {
+		if (cacheptr->num_nodes <= 0)
+			break;
+		cacheptr->gfirst = tmpptr->gnext;
+		if (cacheptr->gfirst != NULL)
+			(cacheptr->gfirst)->gprev = NULL;
+		if (tmpptr->prev == NULL) {
+			hashval = (tmpptr->child) % NUM_LOOKUP_ENTRY;
+			(cacheptr->hashtable[hashval]).first = tmpptr->next;
+		} else {
+			(tmpptr->prev)->next = tmpptr->next;
+		}
+		if (tmpptr->next == NULL) {
+			hashval = (tmpptr->child) % NUM_LOOKUP_ENTRY;
+			(cacheptr->hashtable[hashval]).last = tmpptr->prev;
+		} else {
+			(tmpptr->next)->prev = tmpptr->prev;
+		}
+		free(tmpptr);
+		(cacheptr->num_nodes)--;
+		num_dropped++;
+	}
+	return;
+}
+
+/* Internal function for adding a cache entry. */
+/* Queue the new node to the tail of the ring */
+int add_cache_entry(PATH_CACHE *cacheptr, PATH_LOOKUP *newnode)
+{
+	int ret, errcode;
+	int hashindex;
+
+	ret = sem_wait(&(cacheptr->pathcache_lock));
+	if (ret < 0) {
+		errcode = errno;
+		write_log(0, "Unexpected error. Code %d, %s\n",
+			errcode, strerror(errcode));
+		errcode = -errcode;
+		goto errcode_handle;
+	}
+
+	if (cacheptr->num_nodes >= MAX_LOOKUP_NODES)
+		drop_cache_entry(cacheptr);
+
+	hashindex = (newnode->child) % NUM_LOOKUP_ENTRY;
+
+	/* Change the ring structure */
+	((cacheptr->hashtable[hashindex]).last)->next = newnode;
+	newnode->prev = cacheptr->hashtable[hashindex]).last;
+	newnode->next = NULL;
+	cacheptr->hashtable[hashindex]).last = newnode;
+
+	/* Change the global linked list */
+	(cacheptr->gfirst)->gprev = newnode;
+	newnode->gnext = cacheptr->gfirst;
+	newnode->gprev = NULL;
+	cacheptr->gfirst = newnode;
+	/* TODO: Decide where to actually lock the pathcache lock and where
+		to release */
+	(cacheptr->num_nodes)++;
+	sem_post(&(cacheptr->pathcache_lock));
+	return 0;
+errcode_handle:
+	return errcode;
+}
+
+int delete_pathcache_node(PATH_CACHE *cacheptr, ino_t todelete);
+
+
+int lookup_name(PATH_CACHE *cacheptr, ino_t thisinode, PATH_LOOKUP *retnode);
+
+
+int construct_path_iterate(PATH_CACHE *cacheptr, ino_t thisinode, char **result,
+		int bufsize)
+{
+	char *current_path;
+	char thisname[256];
+	PATH_LOOKUP cachenode;
+	ino_t parent_inode;
+	int ret, pathlen;
+
+	ret = lookup_name(cacheptr, thisinode, &cachenode);
+
+	if (ret < 0) {
+		...
+	}
+
+	parent_inode = cachenode.parent;
+	current_path = calloc(bufsize + strlen(cachenode.childname) + 1,
+				sizeof(char));
+	if (current_path == NULL) {
+		...
+	}
+	pathlen = bufsize + strlen(cachenode.childname) + 1;
+
+/* TODO: Check if the path name exceed the max allowed */
+	snprinf(current_path, pathlen, "%s/%s", cachenode.childname, *result);
+/* TODO: Check if parent inode is root. If so, add the root symbol
+to the path and return */
+	if (parent_inode == cacheptr->root_inode) {
+		...
+	}
 
 }
 
-int construct_path(ino_t thisinode, char **result, int bufsize);
-int lookup_name(ino_t thisinode, char *namebuf);
+int construct_path(PATH_CACHE *cacheptr, ino_t thisinode, char **result);
 
 
 #endif  /* GW20_HCFS_PATH_RECONSTRUCT_H_ */
--- src/HCFS/path_reconstruct.h
+++ src/HCFS/path_reconstruct.h
@@ -14,6 +14,7 @@
 
 #define MAX_LOOKUP_NODES 4096
 #define NUM_LOOKUP_ENTRY 4096
+#define NUM_NODES_DROP 128
 
 struct path_lookup {
 	ino_t child;
@@ -22,25 +23,35 @@ struct path_lookup {
 	long long lookupcount;
 	struct path_lookup *prev;
 	struct path_lookup *next;
+	struct path_lookup *gprev;
+	struct path_lookup *gnext;
 };
 typedef struct path_lookup PATH_LOOKUP;
 
 typedef struct {
 	PATH_LOOKUP *first;
 	PATH_LOOKUP *last;
-	int num_nodes;
 } PATH_HEAD_ENTRY;
 
-PATH_HEAD_ENTRY pathcache[NUM_LOOKUP_ENTRY];
-sem_t pathcache_lock;
+/* There could be multiple such hash table, one for each mounted volume */
+typedef struc {
+	PATH_HEAD_ENTRY hashtable[NUM_LOOKUP_ENTRY];
+	PATH_LOOKUP *gfirst;
+	int num_nodes;
+	sem_t pathcache_lock;
+	ino_t root_inode;
+} PATH_CACHE;
 
 /* API for calling from outside */
-int init_pathcache();
-int destroy_pathcache();
+PATH_CACHE * init_pathcache(ino_t root_inode);
+int destroy_pathcache(PATH_CACHE *cacheptr);
 
-int construct_path(ino_t thisinode, char **result, int bufsize);
-int lookup_name(ino_t thisinode, char *namebuf);
+int delete_pathcache_node(PATH_CACHE *cacheptr, ino_t todelete);
+int lookup_name(PATH_CACHE *cacheptr, ino_t thisinode, PATH_LOOKUP *retnode);
+int construct_path_iterate(PATH_CACHE *cacheptr, ino_t thisinode, char **result,
+		int bufsize);
 
+int construct_path(PATH_CACHE *cacheptr, ino_t thisinode, char **result);
 
 #endif  /* GW20_HCFS_PATH_RECONSTRUCT_H_ */
 
