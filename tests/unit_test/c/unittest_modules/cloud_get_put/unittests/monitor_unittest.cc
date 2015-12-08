7fb33cb9426caa9528caa71a6ca241ed2bdfec0e
Jethro Yu <jethro.yu@hopebaytech.com>
2015-12-08 19:54:33 +0800

    Refactoring unittest

--- tests/unit_test/c/unittest_modules/cloud_get_put/unittests/monitor_unittest.cc
+++ tests/unit_test/c/unittest_modules/cloud_get_put/unittests/monitor_unittest.cc
@@ -26,86 +26,56 @@ class monitorTest : public ::testing::Test {
 
 TEST_F(monitorTest, Backend_Is_Online) {
 	pthread_t monitor_loop_thread;
-	int i;
-	struct timespec req = {0, 90000000};
-	int update_count = 0;
-	int old_status;
+	struct timespec larger_than_interval;
 
 	// Prepare flag to let mock hcfs_test_backend return 200
 	monitoring_interval = 1;
+	larger_than_interval.tv_sec = monitoring_interval;
+	larger_than_interval.tv_nsec = 10000000;
 	hcfs_test_backend_register = 200;
-	hcfs_system->backend_is_online = FALSE;
-	old_status = TRUE;
 
 	// create thread to run monitor loop
 	pthread_create(&monitor_loop_thread, NULL, &monitor_loop, NULL);
-	for (i = 0; i < 30 ; i++) {
-		if (hcfs_system->backend_is_online != old_status) {
-			old_status = hcfs_system->backend_is_online;
-			printf("backend_status %d\n",
-			       hcfs_system->backend_is_online);
-			fflush(stdout);
-		}
-		if (hcfs_system->backend_is_online) {
-			update_count++;
-			if (update_count == 1) {
-				// reset first check
-				puts("reset first monitor check");
-				hcfs_system->backend_is_online = FALSE;
-			} else {
-				// expected second update after monitor interval
-				break;
-			}
-		}
-		nanosleep(&req, NULL);
-	}
+
+	hcfs_system->backend_is_online = FALSE;
+	nanosleep(&larger_than_interval, NULL);
+	ASSERT_EQ(TRUE, hcfs_system->backend_is_online);
+
+	hcfs_system->backend_is_online = FALSE;
+	nanosleep(&larger_than_interval, NULL);
+	ASSERT_EQ(TRUE, hcfs_system->backend_is_online);
+
 	// let system shut down
 	hcfs_system->system_going_down = TRUE;
 	// join thread
 	pthread_join(monitor_loop_thread, NULL);
-	ASSERT_EQ(TRUE, hcfs_system->backend_is_online);
 }
 
 TEST_F(monitorTest, Backend_Is_Offline) {
 	pthread_t monitor_loop_thread;
-	int i;
-	struct timespec req = {0, 90000000};
-	int update_count = 0;
-	int old_status;
+	struct timespec larger_than_interval;
 
 	// Prepare flag to let mock hcfs_test_backend return 401
 	monitoring_interval = 1;
+	larger_than_interval.tv_sec = monitoring_interval;
+	larger_than_interval.tv_nsec = 10000000;
 	hcfs_test_backend_register = 401;
-	hcfs_system->backend_is_online = TRUE;
-	old_status = FALSE;
 
 	// create thread to run monitor loop
 	pthread_create(&monitor_loop_thread, NULL, &monitor_loop, NULL);
-	for (i = 0; i < 30 ; i++) {
-		if (hcfs_system->backend_is_online != old_status) {
-			old_status = hcfs_system->backend_is_online;
-			printf("backend_status %d\n",
-			       hcfs_system->backend_is_online);
-			fflush(stdout);
-		}
-		if (hcfs_system->backend_is_online == FALSE) {
-			update_count++;
-			if (update_count == 1) {
-				// reset first check
-				puts("reset first monitor check");
-				hcfs_system->backend_is_online = TRUE;
-			} else {
-				// expected second update after monitor interval
-				break;
-			}
-		}
-		nanosleep(&req, NULL);
-	}
+
+	hcfs_system->backend_is_online = TRUE;
+	nanosleep(&larger_than_interval, NULL);
+	ASSERT_EQ(FALSE, hcfs_system->backend_is_online);
+
+	hcfs_system->backend_is_online = TRUE;
+	nanosleep(&larger_than_interval, NULL);
+	ASSERT_EQ(FALSE, hcfs_system->backend_is_online);
+
 	// let system shut down
 	hcfs_system->system_going_down = TRUE;
 	// join thread
 	pthread_join(monitor_loop_thread, NULL);
-	ASSERT_EQ(FALSE, hcfs_system->backend_is_online);
 }
 
 TEST_F(monitorTest, Update_Backend_Status_With_Timestamp) {
