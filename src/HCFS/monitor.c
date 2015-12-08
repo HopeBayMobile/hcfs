769121a53a7fcf7b2d702fccf503193c21bb9b2a
Jethro Yu <jethro.yu@hopebaytech.com>
2015-12-08 18:39:22 +0800

    update log format

--- src/HCFS/monitor.c
+++ src/HCFS/monitor.c
@@ -49,10 +49,11 @@ static inline void _write_monitor_loop_status_log(float duration)
 		backend_is_online = hcfs_system->backend_is_online;
 		sync_manual_switch = hcfs_system->sync_manual_switch;
 		sync_paused = hcfs_system->sync_paused;
-		write_log(10, "%s backend %s (time %f), sync switch %s: %s\n",
-			  "Debug: [Monitor]",
-			  backend_is_online ? " online" : "offline", duration,
-			  sync_manual_switch ? " on" : "off",
+		write_log(10, "Debug: [Monitor] backend [%s] (time %.3f)\n",
+			  backend_is_online ? "online" : "offline", duration);
+		write_log(10, "Debug: [Monitor] sync switch [%s]\n",
+			  sync_manual_switch ? "on" : "off");
+		write_log(10, "Debug: [Monitor] hcfs sync state [%s]\n",
 			  sync_paused ? "paused" : "syncing");
 	}
 }
