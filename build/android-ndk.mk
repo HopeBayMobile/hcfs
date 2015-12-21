8a5c7be4db0f0db6607a954f747a460d0a0cb935
Jethro Yu <jethro.yu@hopebaytech.com>
2015-12-21 17:44:39 +0800

    fix NDK_PATH checking

--- build/android-ndk.mk
+++ build/android-ndk.mk
@@ -2,24 +2,27 @@ ifeq "$(ANDROID_NDK_MK_INCLUDED)" ""
 
   TOP := $(dir $(lastword $(MAKEFILE_LIST)))
   NDK_CONFIG := $(TOP).ndk_path
-  -include $(NDK_CONFIG)
-
-  ifeq "$(NDK_PATH)" ""
-	export NDK_PATH := $(shell which ndk-build)
+  ifeq "$(wildcard $(NDK_PATH)/ndk-build)" ""
+    -include $(NDK_CONFIG)
   endif
+  
+  ifeq "$(wildcard $(NDK_PATH)/ndk-build)" ""
+    export NDK_PATH := $(shell which ndk-build)
+  endif
+
   ifneq "$(NDK_PATH)" ""
     export PATH := $(NDK_PATH):$(PATH)
   endif
 
   setup : $(NDK_CONFIG)
-
+ 
   $(NDK_CONFIG) :
   ifeq "$(NDK_PATH)" ""
 	$(error NDK_PATH is not set)
   else
 	@echo "export NDK_PATH:=$(NDK_PATH)" > "$(NDK_CONFIG)"
   endif
-
+  
   .PHONY : setup
 else
   setup :
