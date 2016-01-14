3ccaa7a2d4864cc87898fb86f8eb715a83fc544a
jiahongwu <jiahong.wu@hopebaytech.com>
2016-01-14 18:54:48 +0800

    Copyright fix

--- src/CLI_utils/HCFSvol.c
+++ src/CLI_utils/HCFSvol.c
@@ -1,6 +1,6 @@
 /*************************************************************************
 *
-* Copyright © 2015 Hope Bay Technologies, Inc. All rights reserved.
+* Copyright © 2015-2016 Hope Bay Technologies, Inc. All rights reserved.
 *
 * File Name: HCFSvol.c
 * Abstract: The c source file for CLI utilities
