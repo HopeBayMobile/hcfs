e3b62b8d0c602a3c93256017e7c1fde0bef49081
Jethro Yu <jethro.yu@hopebaytech.com>
2016-01-19 16:58:17 +0800

    Refactorying buildbox

--- containers/android-buildbox/build.sh
+++ containers/android-buildbox/build.sh
@@ -14,7 +14,19 @@ echo -e "\n======== ${BASH_SOURCE[0]} ========"
 repo="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && while [ ! -d .git ] ; do cd ..; done; pwd )"
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $repo/utils/common_header.bash
-cd $repo
+set -e -x
+cd $here
 
-docker build -t docker:5000/android-buildbox .
-docker push docker:5000/android-buildbox
+sudo apt-get install pv wget
+
+IMG=docker:5000/android-buildbox
+TAG=`date +%Y%m%d`
+echo $IMG
+docker build -t $IMG .
+docker tag -f $IMG $IMG:$TAG
+if [ ! -e /usr/local/bin/docker-squash ]; then
+	cd /tmp
+	wget https://github.com/jwilder/docker-squash/releases/download/v0.2.0/docker-squash-linux-amd64-v0.2.0.tar.gz
+	sudo tar -C /usr/local/bin -xzvf docker-squash-linux-amd64-v0.2.0.tar.gz
+fi
+docker push $IMG:$TAG
