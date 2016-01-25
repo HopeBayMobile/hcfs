136c00cf9b4ece40c749a7d73228517b71735544
Jethro Yu <jethro.yu@hopebaytech.com>
2016-01-25 16:10:16 +0800

    update gdb script

--- build/release_resource/install.bat
+++ build/release_resource/install.bat
@@ -1,3 +1,13 @@
+@if "%CD%" == "C:\Windows" (echo ===
+	echo Error: On Windows, use [ net use * \\nas\ubuntu\CloudDataSolution ] to mount here as network drive and execute again.
+	echo ===
+	pause
+	exit)
+@ echo Device will reboot after install hcfs
 adb root
 adb remount
-adb push system /
+adb push hopebay /system/bin
+adb shell busybox --install /system/xbin
+adb push system /system
+adb reboot
+pause
