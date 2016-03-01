8318a6264d7c972217d4265a1641655171c6011c
Jethro Yu <jethro.yu@hopebaytech.com>
2016-03-01 17:22:12 +0800

    mode lz4 module define into third_party/Android.mk

--- build/third_party/Android.mk
+++ build/third_party/Android.mk
@@ -1,5 +1,14 @@
 ifeq "$(INCLUDE_THIRD_PARTY)" ""
-	third_party_dir := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
-	include $(third_party_dir)/jansson/Android.mk
+
+third_party_dir := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
+
+include $(third_party_dir)/jansson/Android.mk
+
+include $(CLEAR_VARS)
+  LOCAL_MODULE := liblz4
+  LOCAL_SRC_FILES := $(wildcard $(third_party_dir)/lz4/lz4.c)
+  LOCAL_C_INCLUDES := $(third_party_dir)/lz4
+include $(BUILD_SHARED_LIBRARY)
+
 endif
 export INCLUDE_THIRD_PARTY := 1
