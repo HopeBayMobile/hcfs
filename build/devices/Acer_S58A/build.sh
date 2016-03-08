6fbcd18923b79e36e0d971ba9a09dbb6cea4ab2d
Jethro Yu <jethro.yu@hopebaytech.com>
2016-03-08 18:26:00 +0800

    sign apk on build

--- build/devices/Acer_S58A/build.sh
+++ build/devices/Acer_S58A/build.sh
@@ -113,6 +113,12 @@ function copy_apk_to_source_tree() {
 function build_system() {
 	{ _hdr_inc - - BUILD_VARIANT $IMAGE_TYPE $FUNCNAME; } 2>/dev/null
 	ssh root@$DOCKER_IP ccache --max-size=30G
+	ssh root@$DOCKER_IP bash -ic "cd /data/device/acer/common/apps/HopebayHCFSmgmt/ && \
+	java  -Xmx2048m -jar /data/out/host/linux-x86/framework/signapk.jar \
+		-w /data/build/target/product/security/platform.x509.pem \
+		/data/build/target/product/security/platform.pk8 \
+		/data/device/acer/common/apps/HopebayHCFSmgmt/${APK_NAME}.apk /data/device/acer/common/apps/HopebayHCFSmgmt/${APK_NAME}-signed.apk && \
+	mv -vf /data/device/acer/common/apps/HopebayHCFSmgmt/${APK_NAME}-signed.apk /data/device/acer/common/apps/HopebayHCFSmgmt/${APK_NAME}.apk"
 	ssh root@$DOCKER_IP bash -ic "./build.sh -s s58a_aap_gen1 -v ${IMAGE_TYPE}"
 }
 function publish_image() {
