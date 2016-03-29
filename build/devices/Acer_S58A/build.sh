d95245b3842453f6479e7f391b81127128129436
Jethro Yu <jethro.yu@hopebaytech.com>
2016-03-29 18:16:44 +0800

    update image build script for new CI flow

--- build/devices/Acer_S58A/build.sh
+++ build/devices/Acer_S58A/build.sh
@@ -9,7 +9,7 @@
 #     PUBLISH_DIR  Absolute path for current branch on nas, start with
 #                     /mnt/CloudDataSolution/TeraFonn_CI_build.
 #
-#     UPSTREAM_LIB_DIR         Absolute path for from previous jenkins job in pipeline.
+#     LIB_DIR         Absolute path for from previous jenkins job in pipeline.
 #
 #     JOB_NAME        image build job name given by jenkins.
 # Revision History
@@ -25,29 +25,6 @@ source $repo/utils/common_header.bash
 pwd;ls -la
 set -e -o functrace
 TRACE="set -x"; UNTRACE="set +x"
-
-echo ========================================
-echo Jenkins pass-through variables:
-echo UPSTREAM_LIB_DIR: ${UPSTREAM_LIB_DIR}
-echo PUBLISH_DIR: ${PUBLISH_DIR}
-echo JOB_NAME: ${JOB_NAME}
-echo ========================================
-echo "Environment variables (with defaults):"
-$TRACE
-
-### Upstream hcfs lib
-# UPSTREAM_LIB_DIR=${UPSTREAM_LIB_DIR:-/mnt/nas/CloudDataSolution/TeraFonn_CI_build/device/s58a_ci/2.0.3.0261/HCFS-android-binary}
-eval '[ -n "$UPSTREAM_LIB_DIR" ]' || { echo Assign these for local build; exit 1; }
-
-### Upstream APK
-APP_NAME=${APP_NAME:-terafonn_1.0.0026}
-APP_DIR=${APP_DIR:-/mnt/nas/CloudDataSolution/HCFS_android/apk_release/$APP_NAME}
-DOCKER_IMAGE='docker:5000/s58a-buildbox:v4.0323-${IMAGE_TYPE}-prebuilt'
-
-### Publish dir
-# PUBLISH_DIR=${PUBLISH_DIR:-/mnt/nas/CloudDataSolution/TeraFonn_CI_build/android-dev/2.0.3.ci.test}
-eval '[ -n "$PUBLISH_DIR" ]' || { echo Assign these for local build; exit 1; }
-JOB_NAME=${JOB_NAME:-HCFS-s58a-image}
 
 #let printf handle the printing
 function _hashes() { printf %0$((${1}))d\\n | tr 0 \# ; }
@@ -74,8 +51,6 @@ function stop_builder() {
 	{ _hdr_inc - - BUILD_VARIANT $IMAGE_TYPE $FUNCNAME $1; } 2>/dev/null
 	docker rm -f $DOCKERNAME || :
 }
-
-alias ssh="ssh -o 'UserKnownHostsFile=/dev/null' -o 'StrictHostKeyChecking=no'"
 function setup_ssh_key() {
 	{ _hdr_inc - - BUILD_VARIANT $IMAGE_TYPE $FUNCNAME; } 2>/dev/null
 	if [ ! -f ~/.ssh/id_rsa.pub ]; then
@@ -101,12 +76,12 @@ function patch_system() {
 function copy_hcfs_to_source_tree() {
 	{ _hdr_inc - - BUILD_VARIANT $IMAGE_TYPE $FUNCNAME; } 2>/dev/null
 	rsync -arcv --no-owner --no-group --no-times -e "ssh -o StrictHostKeyChecking=no" \
-		$UPSTREAM_LIB_DIR/acer-s58a-hcfs/system/ root@$DOCKER_IP:/data/device/acer/s58a/hopebay/
+		$LIB_DIR/acer-s58a-hcfs/system/ root@$DOCKER_IP:/data/device/acer/s58a/hopebay/
 }
 function copy_apk_to_source_tree() {
 	{ _hdr_inc - - BUILD_VARIANT $IMAGE_TYPE $FUNCNAME; } 2>/dev/null
 	rsync -arcv --no-owner --no-group --no-times -e "ssh -o StrictHostKeyChecking=no" \
-		$APP_DIR/${APP_NAME}.apk root@$DOCKER_IP:/data/device/acer/common/apps/HopebayHCFSmgmt/
+		$APP_DIR/*.apk root@$DOCKER_IP:/data/device/acer/common/apps/HopebayHCFSmgmt/
 	rsync -arcv --no-owner --no-group --no-times -e "ssh -o StrictHostKeyChecking=no" \
 		$APP_DIR/arm64-v8a/ root@$DOCKER_IP:/data/device/acer/s58a/hopebay/lib64/
 }
@@ -131,7 +106,6 @@ function publish_image() {
 	rsync -v \
 		root@$DOCKER_IP:/data/out/target/product/s58a/{boot.img,system.img,userdata.img} \
 		${PUBLISH_DIR}/${JOB_NAME}-${IMAGE_TYPE}
-	touch ${PUBLISH_DIR}/${JOB_NAME}-${IMAGE_TYPE}/use_${APP_NAME}
 }
 function mount_nas() {
 	{ _hdr_inc - - Doing $FUNCNAME; } 2>/dev/null
@@ -145,6 +119,25 @@ function unmount_nas() {
 	{ _hdr_inc - - Doing $FUNCNAME; } 2>/dev/null
 	umount /mnt/nas
 }
+
+
+# LIB_DIR=${LIB_DIR:-/mnt/nas/CloudDataSolution/TeraFonn_CI_build/2.0.5.0391-android-dev/HCFS-android-binary}
+# APP_DIR=${APP_DIR:-/mnt/nas/CloudDataSolution/TeraFonn_CI_build/2.0.5.0391-android-dev/HCFS-terafonn-apk}
+# PUBLISH_DIR=${PUBLISH_DIR:-/mnt/nas/CloudDataSolution/TeraFonn_CI_build/0.0.0.ci.test}
+JOB_NAME=${JOB_NAME:-HCFS-s58a-image}
+eval '[ -n "$LIB_DIR" ]' || { echo Error: required parameter LIB_DIR does not exist; exit 1; }
+eval '[ -n "$APP_DIR" ]' || { echo Error: required parameter APP_DIR does not exist; exit 1; }
+eval '[ -n "$PUBLISH_DIR" ]' || { echo Error: required parameter PUBLISH_DIR does not exist; exit 1; }
+DOCKER_IMAGE='docker:5000/s58a-buildbox:v4.0323-${IMAGE_TYPE}-prebuilt'
+
+echo ========================================
+echo Jenkins pass-through variables:
+echo LIB_DIR: ${LIB_DIR}
+echo PUBLISH_DIR: ${PUBLISH_DIR}
+echo JOB_NAME: ${JOB_NAME}
+echo ========================================
+echo "Environment variables (with defaults):"
+$TRACE
 trap stop_builder EXIT # Cleanup docker container
 
 mount_nas
