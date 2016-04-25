fa3b42afe759a21ef1e451a4151deadd3538fcc0
Jethro Yu <jethro.yu@hopebaytech.com>
2016-04-25 15:36:40 +0800

    check read permission of ndk build

--- build/ndk-build.mk
+++ build/ndk-build.mk
@@ -3,20 +3,33 @@ ifeq "$(ANDROID_NDK_MK_INCLUDED)" ""
   TOP := $(dir $(lastword $(MAKEFILE_LIST)))
   NDK_CONFIG := $(TOP).ndk_dir
 
-  # 1. Find correct ndk-build filepath from arguments
-  override NDK_BUILD := $(filter %/ndk-build, $(wildcard \
-	  $(NDK_DIR) \
-	  $(NDK_DIR)/ndk-build \
-	  $(NDK_BUILD) \
-	  $(NDK_BUILD)/ndk-build))
-  override NDK_DIR := $(abspath $(dir $(NDK_BUILD)))
-  # 2. from config
+  define test_ndk_read_permission =
+    ifneq "$$(NDK_DIR)" ""
+      $$(info $$(shell ls $$(NDK_DIR) > /dev/null ))
+    endif
+  endef
+
+# 1. Find correct ndk-build filepath from arguments
+
+  ifneq "$(NDK_DIR)" ""
+    override NDK_BUILD := $(filter %/ndk-build, $(wildcard \
+      $(NDK_DIR) $(NDK_DIR)/ndk-build \
+      $(NDK_BUILD) $(NDK_BUILD)/ndk-build))
+    override NDK_DIR := $(abspath $(dir $(NDK_BUILD)))
+    $(eval $(call test_ndk_read_permission))
+  endif
+
+
+# 2. from config
   ifeq "$(NDK_DIR)" ""
     -include $(NDK_CONFIG)
+    $(eval $(call test_ndk_read_permission))
   endif
-  # 3. from PATH
+
+# 3. from PATH
   ifeq "$(NDK_DIR)" ""
     override NDK_DIR := $(abspath $(dir $(shell /usr/bin/which ndk-build)))
+    $(eval $(call test_ndk_read_permission))
   endif
 
   ifeq "$(wildcard $(NDK_DIR))" ""
