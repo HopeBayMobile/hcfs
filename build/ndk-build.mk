a8c84d9a5cc0b0dc5504a7a1135f3df3fd89322c
Jethro Yu <jethro.yu@hopebaytech.com>
2016-04-25 19:06:49 +0800

    fix ci

--- build/ndk-build.mk
+++ build/ndk-build.mk
@@ -3,14 +3,23 @@ ifeq "$(ANDROID_NDK_MK_INCLUDED)" ""
   TOP := $(dir $(lastword $(MAKEFILE_LIST)))
   NDK_CONFIG := $(TOP).ndk_dir
 
-  # 1. Find correct ndk-build filepath from arguments
-  override NDK_BUILD := $(filter %/ndk-build, $(wildcard \
-	  $(NDK_DIR) \
-	  $(NDK_DIR)/ndk-build \
-	  $(NDK_BUILD) \
-	  $(NDK_BUILD)/ndk-build))
-  override NDK_DIR := $(abspath $(dir $(NDK_BUILD)))
-  # 2. from config
+  define test_ndk_read_permission =
+    ifneq "$$(NDK_DIR)" ""
+      $$(info $$(shell ls $$(NDK_DIR) > /dev/null ))
+    endif
+  endef
+
+# 1. Find correct ndk-build filepath from arguments
+
+  ifneq "$(NDK_DIR)" ""
+    override NDK_DIR := $(filter %/ndk-build, $(wildcard \
+      $(NDK_DIR) $(NDK_DIR)/ndk-build ))
+    override NDK_DIR := $(abspath $(dir $(NDK_DIR)))
+    $(eval $(call test_ndk_read_permission))
+  endif
+
+
+# 2. from config
   ifeq "$(NDK_DIR)" ""
     -include $(NDK_CONFIG)
   endif
@@ -25,7 +34,12 @@ ifeq "$(ANDROID_NDK_MK_INCLUDED)" ""
     # Save NDK_DIR variable
     $(shell echo "override NDK_DIR=$(NDK_DIR)" > $(NDK_CONFIG))
     # Export NDK_DIR into path
-    PATH := $(PATH):$(NDK_DIR)
+    override PATH := $(PATH):$(NDK_DIR)
+  endif
+
+  ifeq "$(wildcard $(NDK_DIR)/toolchains/llvm-3.6/prebuilt/linux-x86_64/bin/ld.mcld)" ""
+    $(info $(NDK_DIR)/toolchains/llvm-3.6/prebuilt/linux-x86_64/bin/ld.mcld)
+    $(error Please do not use r11 or higher to build hcfs, HCFS/Android.mk requires "-fuse-ld=mcld" but mclinker is removed since r11)
   endif
 
 endif
