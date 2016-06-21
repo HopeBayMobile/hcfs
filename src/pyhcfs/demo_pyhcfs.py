d51556eca0804c88016f430ef41691a732f424e0
Jethro Yu <jethro.yu@hopebaytech.com>
2016-06-21 14:29:15 +0800

    Add dummy functions and demo cases for API integration

--- src/pyhcfs/demo_pyhcfs.py
+++ src/pyhcfs/demo_pyhcfs.py
@@ -1,3 +1,30 @@
-from parser import list_external_volume
+# -*- coding: utf-8 -*-
 
-print list_external_volume("testdata/fsmgr")
+import pprint
+from parser import *
+
+_escape = dict((q, dict((c, unicode(repr(chr(c)))[1:-1])
+    for c in range(32) + [ord('\\')] +
+    range(128, 161),
+    **{ord(q): u'\\' + q}))
+    for q in ["'", '"'])
+class MyPrettyPrinter(pprint.PrettyPrinter):
+    def format(self, object, context, maxlevels, level):
+	if type(object) is unicode:
+	    q = "'" if "'" not in object or '"' in object \
+		    else '"'
+	    return ("u" + q + object.translate(_escape[q]) +
+		    q, True, False)
+	return pprint.PrettyPrinter.format(
+		self, object, context, maxlevels, level)
+pp = MyPrettyPrinter(indent=4)
+
+print "\nlist_external_volume"
+pprint.pprint(list_external_volume("testdata/fsmgr"))
+
+print "\nhcfs_parse_meta"
+pprint.pprint(hcfs_parse_meta("testdata/meta123"))
+
+print "\nhcfs_list_dir_inorder"
+offset, resp = hcfs_list_dir_inorder("testdata/meta123", limit=2)
+pp.pprint((offset, resp))
--- src/pyhcfs/parser.py
+++ src/pyhcfs/parser.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # See CFFI docs at https://cffi.readthedocs.org/en/latest/
 from _pyhcfs import ffi, lib
 
@@ -41,8 +42,70 @@ def list_external_volume(fsmgr_path):
     ret = lib.list_external_volume(fsmgr_path, ptr_ret_entry, ret_num)
     if ret != 0:
         return ret
+
     response = []
     for x in range(ret_num[0]):
         entry = ptr_ret_entry[0][x]
         response += [(entry.inode, ffi.string(entry.name))]
+
     return response
+
+def hcfs_parse_meta(meta_path):
+    resp = {
+        'result': 0,
+        'file_type': 2,
+        'child_number': 21,
+        'stat': {
+            'st_dev': '',
+            'st_ino': '22',
+            'st_mode': 'uint32_t',
+            'st_nlink': '',
+            'st_uid': '',
+            'st_gid': '',
+            'st_rdev': '',
+            'st_size': 204800,
+            'st_blksize': 4096,
+            'st_blocks': '',
+            'st_atime': 1465788148,
+            'st_atime_nsec': '',
+            'st_mtime': 1465788148,
+            'st_mtime_nsec': '',
+            'st_ctime': 1465788148,
+            'st_ctime_nsec': ''
+        },
+        'page_offset': 2685
+    }
+
+    return resp
+
+def hcfs_list_dir_inorder(meta_path, offset=(0, 0), limit=20):
+    resp = [
+        (1001, u'Android'),
+        (1002, u'data'),
+        (1003, u'DCIM'),
+        (1004, u'Download'),
+        (1005, u'LINE_Backup'),
+        (1006, u'我的文件夾'),
+        (1007, u'照片 2016'),
+        (1008, u'企劃書 01.docx'),
+        (1009, u'企劃書 02.docx'),
+        (1010, u'活動投影片01.pptx'),
+        (1011, u'活動投影片02.pptx'),
+        (1012, u'照片 5022.jpg'),
+        (1013, u'照片 5023.jpg'),
+        (1014, u'照片 5024.jpg'),
+        (1015, u'照片 5025.jpg'),
+        (1016, u'照片 5026.jpg'),
+        (1017, u'照片 5022.jpg'),
+        (1018, u'照片 5023.jpg'),
+        (1019, u'照片 5024.jpg'),
+        (1020, u'照片 5025.jpg'),
+        (1021, u'照片 5026.jpg'),
+    ]
+
+    offset = (234, 543)
+
+    return (offset, resp)
+
+def get_external_vol(tmp_meta):
+    return list_external_vol(tmp_meta)
