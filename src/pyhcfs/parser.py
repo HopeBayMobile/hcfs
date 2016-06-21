78be974eb20d0811eeee0336065fe1f321fbada3
Jethro Yu <jethro.yu@hopebaytech.com>
2016-06-21 13:53:05 +0800

    let list_external_volume return in format [(inode,name), ...]

--- src/pyhcfs/parser.py
+++ src/pyhcfs/parser.py
@@ -23,23 +23,26 @@ def convert_to_python(s):
     elif type.kind == 'primitive':
         return int(s)
 
-"""
-typedef struct {
-    uint64_t inode;
-    char name[256];
-} PORTABLE_DIR_ENTRY;                                                                
-int32_t list_external_volume(char *meta_path , PORTABLE_DIR_ENTRY **ptr_ret_entry,   
-                             uint64_t *ret_num);                                     
-"""
-
 def list_external_volume(fsmgr_path):
     """
     Return list of hcfs external volumes
 
+    cdef:
+        typedef struct {
+            uint64_t inode;
+            char name[256];
+        } PORTABLE_DIR_ENTRY;
+	int32_t list_external_volume(char *meta_path,
+				     PORTABLE_DIR_ENTRY **ptr_ret_entry,
+				     uint64_t *ret_num);
     """
     ptr_ret_entry = ffi.new("PORTABLE_DIR_ENTRY **")
     ret_num = ffi.new("uint64_t *")
     ret = lib.list_external_volume(fsmgr_path, ptr_ret_entry, ret_num)
     if ret != 0:
         return ret
-    return [convert_to_python(ptr_ret_entry[0][x]) for x in range(ret_num[0])]
+    response = []
+    for x in range(ret_num[0]):
+        entry = ptr_ret_entry[0][x]
+        response += [(entry.inode, ffi.string(entry.name))]
+    return response
