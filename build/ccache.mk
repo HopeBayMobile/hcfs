a628fd2120696f540487392c7d592f16fdee68f5
Jethro Yu <jethro.yu@hopebaytech.com>
2016-06-22 11:10:56 +0800

    Fix ccache including

--- build/ccache.mk
+++ build/ccache.mk
@@ -1,7 +1,7 @@
 ifeq "$(CCACHE_MK_INCLUDED)" ""
-  TOP := $(dir $(lastword $(MAKEFILE_LIST)))
-  CCACHE := /usr/bin/ccache
-  ifneq "$(wildcard $(CCACHE))" ""
+  CCACHE_EXISTS := $(shell ccache -V)
+  ifdef CCACHE_EXISTS
+    override PATH := /usr/lib/ccache:$(PATH)
     export USE_CCACHE := 1
     export CCACHE_BASEDIR=$(shell pwd)
     export CCACHE_UMASK=000
@@ -9,7 +9,7 @@ ifeq "$(CCACHE_MK_INCLUDED)" ""
     export CCACHE_CPP2 := yes
     export CCACHE_COMPILERCHECK := content
 
-    $(shell /usr/bin/ccache -M 12G > /dev/null)
+    $(shell ccache -M 50G > /dev/null)
   else
     $(info ### Yout are not using ccache. Installing ccache can speedup rebuild time about 4x ###)
   endif
