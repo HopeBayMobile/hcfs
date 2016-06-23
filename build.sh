2d68afa94f76e43b76e8754c13ab59a237537e25
Jethro Yu <jethro.yu@hopebaytech.com>
2016-06-23 11:06:06 +0800

    add docker setup routine

--- build.sh
+++ build.sh
@@ -105,7 +105,12 @@ function lib()
 
 function pyhcfs ()
 {
+	$repo/utils/setup_dev_env.sh -v -m docker_host
 	docker pull docker:5000/docker_hcfs_test_slave
+	if ! groups $USER | grep -q "\(docker\|root\)"; then
+		echo To run docker with user, please add your user into docker group and re-login session:
+		echo "sudo usermod -aG docker <user_name>"
+	fi
 	if [ "$TEST" -eq 1 ]; then
 		docker run -it --rm -v "$repo":/hcfs -w="/hcfs" docker:5000/docker_hcfs_test_slave \
 			bash -c "umask 000; python setup.py test"
