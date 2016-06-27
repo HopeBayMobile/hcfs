e489e7e46ae202007b4daf8f807b2d228ea91fbd
Jethro Yu <jethro.yu@hopebaytech.com>
2016-06-27 19:45:09 +0800

    update demo case to show raw returned value

--- src/pyhcfs/demo_pyhcfs.py
+++ src/pyhcfs/demo_pyhcfs.py
@@ -17,13 +17,6 @@ pp.pprint(parse_meta(b"../../tests/unit_test/python/test_nexus_5x/meta"))
 # print "=============================="
 # print "list_dir_inorder"
 ret = { 'offset': (0, 0) }
-sum = 0
-while True:
-    ret = list_dir_inorder(b"../../tests/unit_test/python/test_nexus_5x/meta",
-            ret['offset'], limit=33)
-    files = [ x['d_name'] for x in ret['child_list'] ]
-    if len(files) == 0:
-        break
-    print(files)
-    sum += len(files)
-    print("Total", sum, "files")
+ret = list_dir_inorder(b"../../tests/unit_test/python/test_nexus_5x/meta",
+        ret['offset'], limit=33)
+pp.pprint(ret)
