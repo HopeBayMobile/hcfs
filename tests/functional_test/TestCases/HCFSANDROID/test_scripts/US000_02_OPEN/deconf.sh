ecf59df245a13917156c0c4646005ff0de0f63a9
john.yc.chiang <john.yc.chiang@hopebaytech.com>
2016-07-07 19:35:57 +0800

    integrate HCFS_ANDROID_000_02 & 04

---
+++ tests/functional_test/TestCases/HCFSANDROID/test_scripts/US000_02_OPEN/deconf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+###notation
+##TAG
+deconf() {
+adb shell rm /tmp/hcfs.conf.dec
+adb push hcfsconf /data/
+adb shell chmod 777 /data/hcfsconf
+adb shell /data/hcfsconf dec /data/hcfs.conf /tmp/hcfs.conf.dec
+adb pull /tmp/hcfs.conf.dec /tmp/hcfs.conf.dec
+}
+
+####main####
+deconf
+
