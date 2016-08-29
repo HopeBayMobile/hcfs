459c2ae0be9bedd357e9fcaeea3d4d94286b5784
kewei <kewei.chen@happygorgi.com>
2016-08-29 18:01:03 +0800

    fix bug of cannot return EIO when cache is full and nothing can be replaced

--- src/HCFS/hcfs_cacheops.c
+++ src/HCFS/hcfs_cacheops.c
@@ -591,11 +591,23 @@ void run_cache_loop(void)
 int32_t sleep_on_cache_full(void)
 {
 	int32_t cache_replace_status;
+	int32_t num_replace;
 
 	/* Check cache replacement status */
 	cache_replace_status = hcfs_system->systemdata.cache_replace_status;
-	if (cache_replace_status < 0)
+	if (cache_replace_status < 0) {
 		return cache_replace_status;
+	} else {
+		sem_getvalue(&(hcfs_system->something_to_replace),
+				&num_replace);
+		/* In case of network disconn and nothing can be replaced,
+		 * ask cache manager to traverse all blocks one more time
+		 * and set cache status again. */
+		if (hcfs_system->sync_paused == TRUE &&
+				num_replace == 0) {
+			sem_post(&(hcfs_system->something_to_replace));
+		}
+	}
 
 	sem_post(&(hcfs_system->num_cache_sleep_sem)); /* Count++ */
 	sem_wait(&(hcfs_system->check_cache_sem)); /* Sleep a while */
