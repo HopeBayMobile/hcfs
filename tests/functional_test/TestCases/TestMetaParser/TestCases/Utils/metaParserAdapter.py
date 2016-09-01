1ffb04fe40f068fd09d5f2e1090a84ce878d6539
ted.chen <ted.chen@hopebaytech.com>
2016-09-01 09:46:54 +0800

    Function call log in metaParserAdapter.py

--- tests/functional_test/TestCases/TestMetaParser/TestCases/Utils/metaParserAdapter.py
+++ tests/functional_test/TestCases/TestMetaParser/TestCases/Utils/metaParserAdapter.py
@@ -1,51 +1,80 @@
 import subprocess
 from subprocess import Popen, PIPE
 import ast
+import os
+import shutil
 
-import config
-
-logger = config.get_logger().getChild(__name__)
+THIS_DIR = os.path.abspath(os.path.dirname(__file__))
+REPORT_DIR = os.path.join(THIS_DIR, "report")
+if os.path.exists(REPORT_DIR):
+    shutil.rmtree(REPORT_DIR)
+os.makedirs(REPORT_DIR)
 
 
 def list_external_volume(path):
-    cmd = "python3 -c 'from pyhcfs.parser import *;import sys;sys.stdout.write(repr(list_external_volume(b\"" + path + "\")))'"
-    pipe = subprocess.Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-    out, err = pipe.communicate()
-    logger.debug("list_external_volume" + repr((cmd, out, err)))
-    return ast.literal_eval(out)
+    return _python3_call("list_external_volume", (path,))
 
 
 def parse_meta(path):
-    cmd = "python3 -c 'from pyhcfs.parser import *;import sys;sys.stdout.write(repr(parse_meta(b\"" + path + "\")))'"
-    pipe = subprocess.Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-    out, err = pipe.communicate()
-    logger.debug("parse_meta" + repr((cmd, out, err)))
-    return ast.literal_eval(out)
+    return _python3_call("parse_meta", (path,))
 
 
 def list_dir_inorder(path, offset=(0, 0), limit=1000):
-    cmd = "python3 -c 'from pyhcfs.parser import *;import sys;sys.stdout.write(repr(list_dir_inorder(b\"" + path + "\", " + str(
-        offset) + ", " + str(limit) + ")))'"
-    pipe = subprocess.Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-    out, err = pipe.communicate()
-    logger.debug("list_dir_inorder" + repr((cmd, out, err)))
-    return ast.literal_eval(out)
+    return _python3_call("list_dir_inorder", (path, offset, limit))
 
 
 def get_vol_usage(path):
-    cmd = "python3 -c 'from pyhcfs.parser import *;import sys;sys.stdout.write(repr(get_vol_usage(b\"" + path + "\")))'"
-    pipe = subprocess.Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-    out, err = pipe.communicate()
-    logger.debug("get_vol_usage" + repr((cmd, out, err)))
-    return ast.literal_eval(out)
+    return _python3_call("get_vol_usage", (path,))
 
 
 def list_file_blocks(path):
-    cmd = "python3 -c 'from pyhcfs.parser import *;import sys;sys.stdout.write(repr(list_file_blocks(b\"" + path + "\")))'"
+    return _python3_call("list_file_blocks", (path,))
+
+################################## private ####################################
+
+
+def _python3_call(func, args):
+    param = ""
+    if len(args) != 0:
+        for arg in args[:-1]:
+            param += _arg_str(arg) + ","
+        param += _arg_str(args[-1])
+
+    imports = "from pyhcfs.parser import *;import sys;"
+    func_call = "{0}({1})".format(func, param)
+    write_stdout = "sys.stdout.write(repr({0}))".format(func_call)
+    cmd = "python3 -c '{0}{1}'".format(imports, write_stdout)
+
     pipe = subprocess.Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
     out, err = pipe.communicate()
-    logger.debug("list_file_blocks" + repr((cmd, out, err)))
-    return ast.literal_eval(out)
+    _log_cmd(func, cmd, out, err)
+    result = ast.literal_eval(out)
+    _log_fun(func, args, result)
+    return result
+
+
+def _arg_str(arg):
+    if isinstance(arg, basestring):
+        return "b\"" + arg + "\""
+    else:
+        return str(arg)  # danger for  str in list
+
+
+def _log_fun(name, args, result):
+    file_path = os.path.join(REPORT_DIR, name)
+    with open(file_path, "a") as log_file:
+        msg = "{0}{1}={2}\n".format(str(name), repr(args), str(result))
+        log_file.write(msg)
+
+
+def _log_cmd(file_name, cmd, out, err):
+    file_path = os.path.join(REPORT_DIR, file_name)
+    with open(file_path, "a") as log_file:
+        log_file.write("-" * 120)
+        log_file.write("\n")
+        msg = "cmd:{0}\nout:{1}\nerr:{2}\n".format(
+            str(cmd), str(out), str(err))
+        log_file.write(msg)
 
 if __name__ == '__main__':
     path = "../test_data_v2/FSstat0"
@@ -56,8 +85,8 @@ if __name__ == '__main__':
     result = get_vol_usage(path)
     print "path:" + path
     print repr(result)
-    path = "../test_data_v2/FSstat3"
-    result = get_vol_usage(path)
+    path = "../test_data_v2/248/meta_248"
+    result = list_dir_inorder(path)
     print "path:" + path
     print repr(result)
     path = "../test_data_v2/FSstat147"
