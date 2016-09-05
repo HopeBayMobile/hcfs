ade06e5092633803a698c8ce359325289219c72d
wuxian.zhang <wuxian.zhang@happygorgi.com>
2016-09-05 18:40:30 +0800

    Add 6 test cases for list_volume

--- src/pyhcfs/demo_pyhcfs.py
+++ src/pyhcfs/demo_pyhcfs.py
@@ -17,11 +17,29 @@ def demo(cmd):
 
 test_target='test_data/v1/android'
 
-demo_title("list_external_volume")
-demo('list_external_volume(b"'+test_target+'/fsmgr")')
+demo_title("list_volume")
+demo('list_volume(b"'+test_target+'/fsmgr")')
 
-demo_title("list_external_volume (Failure)")
-demo('list_external_volume(b"")')
+demo_title("list_volume (Failure)")
+demo('list_volume(b"")')
+
+# demo_title("list_volume (Failure data_1000_0_15)")
+# demo('list_volume(b"'+test_target+'/random/data_1000_0_15")')
+
+# demo_title("list_volume (Failure empty)")
+# demo('list_volume(b"'+test_target+'/random/empty")')
+
+# demo_title("list_volume (Success FSmgr_backup))")
+# demo('list_volume(b"'+test_target+'/random/FSmgr_backup")')
+
+# demo_title("list_volume (Failure FSstat0))")
+# demo('list_volume(b"'+test_target+'/random/FSstat0")')
+
+# demo_title("list_volume (Failure meta_10))")
+# demo('list_volume(b"'+test_target+'/random/meta_10")')
+ 
+# demo_title("list_volume (Failure random)")
+# demo('list_volume(b"'+test_target+'/random/random")')
 
 demo_title("parse_meta")
 demo('parse_meta(b"'+test_target+'/meta_isdir")')
@@ -35,9 +53,6 @@ demo('list_dir_inorder(b"'+test_target+'/meta_isdir", ret["offset"], limit=100)'
 
 demo_title("get_vol_usage")
 demo('get_vol_usage(b"'+test_target+'/FSstat")')
-
-demo_title("get_vol_usage (Failure)")
-demo('get_vol_usage(b"'+test_target+'/FSstat_rand")')
 
 demo_title("list_file_blocks")
 demo('list_file_blocks(b"'+test_target+'/meta_isreg")')
