0392b2d0ae911fa6d1ee9838d9940ddddaaf8ff0
ted.chen <ted.chen@hopebaytech.com>
2016-09-05 13:51:57 +0800

    Remove unused file

--- tests/functional_test/TestCases/TestMetaParser/test_report_perm_change.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo -e "\n======== ${BASH_SOURCE[0]} ========"
-repo="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && while [ ! -d .git ] ; do cd ..; done; pwd )"
-here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-sudo chmod 777 -R $here/TestCases/Utils/report
-sudo chmod 777 -R $repo/tests/functional_test/Reports
