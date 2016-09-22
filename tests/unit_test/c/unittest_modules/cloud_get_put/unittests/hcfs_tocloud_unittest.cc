346bba26f532af8be09b49f8fb226d2948435036
Jim Huang <jim.huang@hopebaytech.com>
2016-09-22 12:14:43 +0800

    tests: cloud_get_put: avoid double free in destructor
    The singleton pattern is used for InitUploadControlTool, but its
    destructor might be invoked twice, which causes doubly free the instance
    of internal tool. We can avoid it by manually calling the destructor.

--- tests/unit_test/c/unittest_modules/cloud_get_put/unittests/hcfs_tocloud_unittest.cc
+++ tests/unit_test/c/unittest_modules/cloud_get_put/unittests/hcfs_tocloud_unittest.cc
@@ -291,11 +291,13 @@ public:
 	}
 private:
 	InitUploadControlTool() {}
-	~InitUploadControlTool()
+	static InitUploadControlTool *tool;
+public:
+	static void Destruct()
 	{
 		delete tool;
+		tool = NULL;
 	}
-	static InitUploadControlTool *tool;
 };
 
 
@@ -328,6 +330,8 @@ protected:
 		nftw(tmppath, do_delete, 20, FTW_DEPTH);
 		close(InitUploadControlTool::Tool()->fd);
 		unlink(InitUploadControlTool::Tool()->progress_path);
+
+		InitUploadControlTool::Destruct();
 
 		unlink(MOCK_META_PATH);
 	}
