7b9e87aa89c1fbe966573a89aa56b6eba0ef0ff7
kewei <kewei.chen@happygorgi.com>
2016-10-06 10:41:10 +0800

    block iterator

--- src/HCFS/block_iterator.c
+++ src/HCFS/block_iterator.c
@@ -14,34 +14,49 @@ FILE_BLOCK_ITERATOR *init_block_iter(FILE *fptr)
 	FREAD(&(iter->filestat), sizeof(HCFS_STAT), 1, fptr);
 	FREAD(&(iter->filemeta), sizeof(FILE_META_TYPE), 1, fptr);
 
-	total_blocks = iter->filestat.size == 0 ? 0 : ((iter->filestat.size - 1)
-			/ MAX_BLOCK_SIZE) + 1;
-	
-	
-	for (count = 0; count < total_blocks; count++) {
-		e_index = count % MAX_BLOCK_ENTRIES_PER_PAGE;
-		which_page = count / MAX_BLOCK_ENTRIES_PER_PAGE;
-		page_pos = seek_page2(&(iter->filemeta), fptr, which_page, 0);
-		if (page_pos <= 0)
-			count += (MAX_BLOCK_ENTRIES_PER_PAGE - 1);
-		else
-			break;
-	}
-	iter->page_pos = page_pos;
-	iter->page_index = page_index;
-	iter->e_index = 0;
+	iter->total_blocks = iter->filestat.size == 0 ? 0 :
+			((iter->filestat.size - 1) / MAX_BLOCK_SIZE) + 1;
+	iter->now_block = -1;
 	iter->fptr = fptr;
+	iter->begin = begin_block;
+	iter->next = next_block;
+	iter->jump = goto_block;
 
+	return iter;
 }
 
 void destroy_block_iter(FILE_BLOCK_ITERATOR *iter)
 {
-
+	free(iter);
 }
 
 FILE_BLOCK_ITERATOR *next_block(FILE_BLOCK_ITERATOR *iter)
 {
+	int64_t count, which_page;
+	int32_t e_index;
 
+	for (count = iter->now_block + 1; count < total_blocks; count++) {
+		e_index = count % MAX_BLOCK_ENTRIES_PER_PAGE;
+		which_page = count / MAX_BLOCK_ENTRIES_PER_PAGE;
+		if (which_page == iter->now_page)
+			break;
+		page_pos = seek_page2(&(iter->filemeta), fptr, which_page, 0);
+		if (page_pos <= 0) {
+			count += (MAX_BLOCK_ENTRIES_PER_PAGE - 1);
+		} else {
+			FSEEK(fptr, page_pos, SEEK_SET);
+			FREAD(&(iter->page), sizeof(BLOCK_ENTRY_PAGE), 1, fptr);
+			break;
+		}
+	}
+	if (count >= total_blocks)
+		return NULL;
+
+	iter->page_pos = page_pos;
+	iter->now_page = which_page;
+	iter->e_index = e_index;
+	iter->now_block = count;
+	return iter;
 }
 
 FILE_BLOCK_ITERATOR *goto_block(FILE_BLOCK_ITERATOR *iter, int64_t block_no)
--- src/HCFS/block_iterator.h
+++ src/HCFS/block_iterator.h
@@ -9,12 +9,16 @@
 
 typedef struct FILE_BLOCK_ITERATOR {
 	int64_t page_pos;
-	int64_t page_index;
+	int64_t now_page;
 	int32_t e_index;
+	int64_t now_block;
+	int64_t total_blocks;
 	HCFS_STAT filestat;
 	FILE_META_TYPE filemeta;
 	BLOCK_ENTRY_PAGE page;
 	FILE *fptr;
+	FILE_BLOCK_ITERATOR *(*next)(FILE_BLOCK_ITERATOR *iter);
+	FILE_BLOCK_ITERATOR *(*jump)(FILE_BLOCK_ITERATOR *iter, int64_t block_no);
 } FILE_BLOCK_ITERATOR;
 
 FILE_BLOCK_ITERATOR *init_block_iter(FILE *fptr);
