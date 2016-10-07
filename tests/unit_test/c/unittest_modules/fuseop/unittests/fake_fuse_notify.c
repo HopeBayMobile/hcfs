52ff80ee794ea8b6fb5d6a7d2523547145eeaf9b
ripley.chu <ripley.chu@hopebaytech.com>
2016-10-07 15:20:12 +0800

    Fix unittest error: Not found hfuse_ll_notify_delete().

--- tests/unit_test/c/unittest_modules/fuseop/unittests/fake_fuse_notify.c
+++ tests/unit_test/c/unittest_modules/fuseop/unittests/fake_fuse_notify.c
@@ -1,5 +1,12 @@
 #include <fuse/fuse_lowlevel.h>
 
+int32_t hfuse_ll_notify_delete(struct fuse_chan *ch,
+			    fuse_ino_t parent,
+			    fuse_ino_t child,
+			    const char *name,
+			    size_t namelen)
+{
+}
 void hfuse_ll_notify_delete_mp(struct fuse_chan *ch,
 			       fuse_ino_t parent,
 			       fuse_ino_t child,
