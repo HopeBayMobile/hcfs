3b7cf4ae3938935f16a3e14fd8771a5c81b16434
Jethro Yu <jethro.yu@hopebaytech.com>
2016-11-15 10:09:29 +0000

    Refactoring

--- build/internal_testing_resource/gdb.sh
+++ build/internal_testing_resource/gdb.sh
@@ -1,136 +1,183 @@
 #!/bin/bash
-# vim:set tabstop=4 shiftwidth=4 softtabstop=0 expandtab:
-set -e
-# Usage Info
+# Load Default Value
+: "${TARGET_ARCH:=64}"
 
-NDK_PATH=`find /opt -maxdepth 1 -name 'android-ndk-r*'`
-TARGET_ARCH=64
 
 ErrorReport() {
-  local script="$1"
-  local parent_lineno="$2"
-  local message="$3"
-  local code="${4:-1}"
-  echo "Error near ${script} line ${parent_lineno}; exiting with status ${code}"
-  if [[ -n "$message" ]] ; then
-    echo -e "Messageecho ${message}"
-  fi
-  exit "${code}"
+	local script="$1"
+	local parent_lineno="$2"
+	local code="${3:-1}"
+	eval printf %.0s- '{1..'"${COLUMNS:-$(tput cols)}"\}; echo
+	echo "Error is near ${script} line ${parent_lineno}. Return ${code}"
+	local Start=$((parent_lineno-2))
+	if [[ $Start -lt 1 ]]; then
+		Start=$parent_lineno
+		Point=1
+	fi
+	local End=$((parent_lineno+2))
+	cat -n "${script}" \
+		| sed -n "${Start},${End}p" \
+		| sed $Point"s/^  / >/"
+	eval printf %.0s- '{1..'"${COLUMNS:-$(tput cols)}"\}; echo
+	exit "${code}"
 }
 
 # Enable error trace
-trap 'ErrorReport "${BASH_SOURCE[0]}" ${LINENO}' ERR
-set -e -o errtrace
+trap 'ErrorReport "${BASH_SOURCE[0]}" ${LINENO} $?' ERR
+set -o pipefail  # trace ERR through pipes
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
 Usage() {
-    local _prog=$(basename $0)
-    echo "Usage: $_prog [option]..."
-    echo "option:"
-    echo "  -push: set the SDK path (Default: $SDK_PATH)"
-    echo "  -sdk <sdk path>: set the SDK path (Default: $SDK_PATH)"
-    echo "  -h: show this usage"
-    [ "$1" = 0 ] && exit 0 || exit 1
+    local _prog
+    _prog="$(basename "$0")"
+    cat <<EOU
+NAME
+    gdb.sh - Start GDB session to debug hcfs on Android device
+
+SYNOPSIS
+    $_prog [option]...
+
+DESCRIPTION
+    -h
+        Show this usage
+
+    -push
+        Push hcfs and .so files into device and reboot
+
+    -pushonly
+        Push hcfs and .so files into device only
+
+	-ndk <NDK_PATH>
+			set the NDK path
+EOU
+	exit ${1:-0}
 }
 
 CheckParams() {
-    PUSHONLY=0
-    PUSH=0
-    while [ "$1" != "" ]; do
-        case $1 in
-        -h)        Usage 0 ;;
-        -ndk)      NDK_PATH=$2; shift ;;
-        -push)     PUSH=1 ;;
-        -pushonly) PUSHONLY=1 ;;
-        *)         Usage 1 ;;
-        esac
-        shift
-    done
+	PUSHONLY=0
+	PUSH=0
+	while [ "${1:-}" != "" ]; do
+		case $1 in
+		-h)        Usage 0;;
+		-ndk)
+			if [ $# -lt 2 ]; then
+				echo "Usage: -ndk <NDK_PATH>"
+				exit 1
+			fi
+			NDK_PATH_SET="$2"
+			shift ;;
+		-push)     PUSH=1;;
+		-pushonly) PUSHONLY=1;;
+		*)         echo ls: invalid option -- \'"$1"\'; Usage 1;;
+		esac
+		shift
+	done
 
-    # set PATH
-    export PATH=$NDK_PATH/prebuilt/linux-x86_64/bin:$PATH
+	# set PATH
+	if [ -z "${NDK_PATH_SET:=}" -a -z "$NDK_PATH" ] ; then
+		echo "Error: require NDK_PATH, use -ndk <NDK_PATH> OR export NDK_PATH before execute script"
+		exit 1
+	fi
+	if [ -n "$NDK_PATH" ]; then
+		export PATH=$NDK_PATH/prebuilt/linux-x86_64/bin:$PATH
+	fi
+	if [ -n "$NDK_PATH_SET" ]; then
+		export PATH=$NDK_PATH_SET/prebuilt/linux-x86_64/bin:$PATH
+	fi
 }
 
 CheckProgram() {
-    local _tool=$1
-    which "$_tool" > /dev/null 2>&1 || echo "$ERROR_HDR $_tool is not found. Please install it or add its path in \$PATH."
+	local _tool=$1
+	which "$_tool" > /dev/null 2>&1 || echo "$ERROR_HDR $_tool is not found. Please install it or add its path in \$PATH."
 }
 
 CheckTools() {
-    local _ret=0
-    if ! CheckProgram adb; then
-        echo "   Or, install adb by 'apt-get install android-tools-adb'"
-        _ret=1
-    fi
-    if ! CheckProgram fastboot; then
-        echo "   Or, install fastboot by 'apt-get install android-tools-fastboot'"
-        _ret=1
-    fi
+	local _ret=0
+	if ! CheckProgram adb; then
+		echo "   Or, install adb from android-sdk"
+		_ret=1
+	fi
+	if ! CheckProgram fastboot; then
+		echo "   Or, install adb from android-sdk"
+		_ret=1
+	fi
 
-    if ! CheckProgram gdb; then
-        _ret=1
-    elif ! gdb --configuration | grep -q multiarch; then
-        echo ""
-        echo "Error: gdb doesn't include ndk data, please use -ndk pass android-ndk-r12+ path"
-        echo Gdb path found in script: `which gdb`
-        _ret=1
-    fi
-    if which cgdb > /dev/null 2>&1; then
-        GDB=cgdb
-    else
-        GDB=gdb
-    fi
+	if ! CheckProgram gdb; then
+		_ret=1
+	elif ! gdb --configuration | grep -q multiarch; then
+		which gdb
+		echo ""
+		echo "Error: gdb doesn't include ndk data, please use -ndk pass android-ndk-r12+ path"
+		echo Gdb path found in script: "$(which gdb)"
+		_ret=1
+	fi
+		which gdb
+	if which cgdb > /dev/null 2>&1; then
+		GDB=cgdb
+	else
+		GDB=gdb
+	fi
 
-    return $_ret
+	return $_ret
 }
 
 PullGDBFiles() {
-    echo [PullGDBFiles]
-    adb wait-for-device
-    SRC=system/bin
-    for i in linker${TARGET_ARCH};
-    do
-        [ ! -f ./$SRC/$i ] && adb pull /$SRC/$i ./$SRC/ || :
-    done
+	echo ">> [PullGDBFiles]"
+	adb wait-for-device
+	SRC=system/bin
+	for i in linker${TARGET_ARCH};
+	do
+		if [ ! -f "./$SRC/$i" ]; then
+			adb pull "/$SRC/$i" "./$SRC/"
+		fi
+	done
 
-    SRC=system/lib${TARGET_ARCH}
-    for i in libcurl.so libcrypto.so libsqlite.so libfuse.so libjansson.so \
-        libstdc++.so libm.so libc.so libssl.so libz.so libc++.so liblog.so \
-        libicuuc.so libicui18n.so libutils.so libbacktrace.so libcutils.so \
-        libbase.so libunwind.so libnetd_client.so;
-    do
-        [ ! -f ./$SRC/$i ] && adb pull /$SRC/$i ./$SRC/ || :
-    done
+	echo ">>   Check required library files. Pull files if they are missing or mismatch"
+	CHANGELIST=$(ANDROID_PRODUCT_OUT=. adb sync -l system 2>&1)
+	SRC=system/lib${TARGET_ARCH}
+	for i in libcrypto.so libsqlite.so libstdc++.so libm.so libc.so libssl.so \
+	libz.so libc++.so liblog.so libicuuc.so libicui18n.so libutils.so \
+	libbacktrace.so libcutils.so libbase.so libunwind.so libnetd_client.so;
+	do
+		if [[ ! -f ./$SRC/$i || $CHANGELIST = *$i* ]]; then
+			adb pull -a "/$SRC/$i" "./$SRC/" || :
+		fi
+	done
 }
 
 PushGDBbinary() {
-    echo [PushGDBbinary]
-    adb wait-for-device
-    adb root
-    adb wait-for-device
-    if ! (adb disable-verity | grep -q already); then
-        adb reboot
-        adb wait-for-device
-        adb root
-    fi
-    adb remount
-    adb push system/bin/hcfs /system/bin/
-    for so in `find system/lib${TARGET_ARCH} -type f -perm 755`;
-    do
-        adb push $so /system/lib${TARGET_ARCH}/
-    done
-    adb shell 'set `ps | grep /system/bin/hcfs`; su root kill $2'&
-    adb reboot
+	echo ">> [PushGDBbinary]"
+	adb wait-for-device
+	adb root
+	adb wait-for-device
+	if ! adb disable-verity | grep -q already; then
+		echo ">>   Disable-verity and reboot"
+		adb reboot
+		adb wait-for-device
+		adb root
+	fi
+	adb remount
+	echo ">>   Push hcfs"
+	OUT=$(ANDROID_PRODUCT_OUT=. adb sync system)
+	if [[ ! $OUT = *"0 files pushed"* ]]; then
+		echo ">>   Some files pushed, Reboot"
+		adb shell 'set `ps | grep /system/bin/hcfs`; su root kill $2'&
+		adb reboot
+	fi
 }
 
 StartGDB() {
-    echo [StartGDB]
-    adb wait-for-device
-    adb shell 'set `ps | grep /system/bin/hcfs`; su root gdbserver'${TARGET_ARCH}' --attach :5678 $2'&
-    gdbserverpid=$!
-    sleep 1
-    adb forward tcp:5678 tcp:5678
-    $GDB -x gdb.setup
-    kill $gdbserverpid || :
+	echo ">> [StartGDB]"
+	adb wait-for-device
+	adb forward tcp:5678 tcp:5678
+	adb shell 'set `ps | grep gdbserver`; [ -n "$2" ] && su root kill $2'
+	adb shell 'set `ps | grep /system/bin/hcfs`; su root gdbserver --attach :5678 $2'&
+	gdbserverpid=$!
+	sleep 1
+	$GDB -x gdb.setup
+	kill $gdbserverpid || :
 }
 
 # Check parameters
@@ -141,11 +188,12 @@ CheckTools
 
 # Main scripts
 
-if [[ $PUSH = 1 || $PUSHONLY = 1 ]]; then
-    PushGDBbinary
-fi
-if [[ $PUSHONLY == 1 ]]; then
-    exit 0
-fi
 PullGDBFiles
-StartGDB
+if [[ $PUSH = 1 || $PUSHONLY = 1 ]]; then
+	PushGDBbinary
+fi
+if [[ $PUSHONLY != 1 ]]; then
+	StartGDB
+fi
+
+# vim:set tabstop=4 shiftwidth=4 softtabstop=0 noexpandtab:
