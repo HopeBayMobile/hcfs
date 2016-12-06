7a85252d8fd70acf8a281336cd21a1ee21eeac07
jiahongwu <jiahong.wu@hopebaytech.com>
2016-12-06 17:47:05 +0800

    Adding notification routine for inval_entry

--- src/HCFS/fuse_notify.h
+++ src/HCFS/fuse_notify.h
@@ -7,6 +7,7 @@
 *
 * Revision History
 * 2016/8/25 Jethro Add fuse_notify.h
+* 2016/12/6 Jiahong Adding inval_entry notification
 *
 **************************************************************************/
 #ifndef SRC_HCFS_FUSE_NOTIFY_H_
@@ -19,10 +20,10 @@
 
 #define FUSE_NOTIFY_RINGBUF_MAXLEN 1024
 
-typedef enum NOTIFY_FUNCTION { NOOP, DELETE } NOTIFY_FUNCTION;
+typedef enum NOTIFY_FUNCTION { NOOP, DELETE, INVAL_ENT } NOTIFY_FUNCTION;
 typedef enum NOTIFY_ACTION { RUN, DESTROY_BUF } NOTIFY_ACTION;
 
-/* Actual notify data defenitions */
+/* Actual notify data definitions */
 typedef struct {
 	NOTIFY_FUNCTION func;
 	struct fuse_chan *ch;
@@ -31,6 +32,14 @@ typedef struct {
 	char *name;
 	size_t namelen;
 } _PACKED FUSE_NOTIFY_DELETE_DATA;
+
+typedef struct {
+	NOTIFY_FUNCTION func;
+	struct fuse_chan *ch;
+	fuse_ino_t parent;
+	char *name;
+	size_t namelen;
+} _PACKED FUSE_NOTIFY_INVAL_ENT_DATA;
 
 /* Ring Buffer */
 
@@ -63,6 +72,7 @@ typedef struct {
 typedef int32_t(fuse_notify_fn)(FUSE_NOTIFY_PROTO *, enum NOTIFY_ACTION);
 fuse_notify_fn _do_hfuse_ll_notify_noop;
 fuse_notify_fn _do_hfuse_ll_notify_delete;
+fuse_notify_fn _do_hfuse_ll_notify_inval_ent;
 
 int32_t init_notify_buf(void);
 void destory_notify_buf(void);
@@ -83,5 +93,9 @@ int32_t hfuse_ll_notify_delete_mp(struct fuse_chan *ch,
 			       const char *name,
 			       size_t namelen,
 			       const char *selfname);
+int32_t hfuse_ll_notify_inval_ent(struct fuse_chan *ch,
+			    fuse_ino_t parent,
+			    const char *name,
+			    size_t namelen);
 
 #endif /* SRC_HCFS_FUSE_NOTIFY_H_ */
