ce425abcb0afb5cff18cefb11fbc3b18c3167cee
kewei <kewei.chen@happygorgi.com>
2016-12-12 18:33:07 +0800

    add ut

---
+++ tests/unit_test/c/unittest_modules/utility_function/unittests/hash_list_mock_params.h
@@ -0,0 +1,11 @@
+#include <inttypes.h>
+
+#define TEST_HASH_SIZE 5566
+
+typedef struct TEST_KEY {
+	int32_t key;
+} TEST_KEY;
+
+typedef struct TEST_DATA {
+	int32_t data;
+} TEST_DATA;
---
+++ tests/unit_test/c/unittest_modules/utility_function/unittests/hash_list_struct_mock_ftn.c
@@ -0,0 +1,6 @@
+#include <inttypes.h>
+
+int32_t write_log(int32_t level, const char *format, ...)
+{
+	return 0;
+}
