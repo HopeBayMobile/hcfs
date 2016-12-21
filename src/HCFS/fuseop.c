9006b048f4a8abcf11d95def731797ad3e4d0b0f
Jim Huang <jim.huang@hopebaytech.com>
2016-12-21 16:26:00 +0800

    fuseop: avoid dereferencing a null pointer
    *body_ptr is not validated after the return of helper function.
    The present use scenario is a bit dangerous though, and we should encapsulate
    memory operation within temporal isolated access.

--- src/HCFS/fuseop.c
+++ src/HCFS/fuseop.c
@@ -3071,6 +3071,8 @@ int32_t truncate_truncate(ino_t this_inode,
 				       last_index);
 	if (ret < 0)
 		return ret;
+	if (!*body_ptr)
+		return -EINVAL;
 
 	ret = fetch_block_path(thisblockpath, filestat->ino, last_block);
 	if (ret < 0)
