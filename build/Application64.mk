8ea9a54f68a22b89a5fe24bb8ec8e90cdbd244b6
Jiahong Wu <jiahong.wu@hopebaytech.com>
2017-03-28 16:16:08 +0800

    Modified Makefile so that both nexus 5x and nexus 7 HCFS binaries can be built in one pass

---
+++ build/Application64.mk
@@ -0,0 +1,15 @@
+#APP_OPTIM := release
+APP_PLATFORM := android-21
+#APP_STL := gnustl_static
+#APP_CPPFLAGS += -frtti
+#APP_CPPFLAGS += -fexceptions
+#APP_CPPFLAGS += -DANDROID
+#APP_CPPFLAGS += -DANDROID_ENV
+#APP_ABI := armeabi-v7a
+#APP_ABI := armeabi
+APP_ABI := arm64-v8a
+#APP_ABI := arm64-v8a armeabi armeabi-v7a 
+#APP_ABI := arm64-v8a x86_64 mips64 armeabi armeabi-v7a x86 mips
+#APP_MODULES := libfuse libssl libcrypto libcurl hcfs
+#NDK_TOOLCHAIN_VERSION := clang
+APP_BUILD_SCRIPT := Android.mk
